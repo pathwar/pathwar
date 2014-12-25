3aa014fe2caa8c758369e2b39f0e8ebf147cf6c4
Manfred Touron <m@42.am>
2014-12-25 21:05:14 +0100

    Added a PHP/Sqlite example. Fixes #3

---
+++ challenges/core/templates/apache-php/examples/php-sqlite/www/phpliteadmin.config.php
@@ -0,0 +1,79 @@
+<?php
+//
+// This is sample configuration file
+//
+// You can configure phpliteadmin in one of 2 ways:
+// 1. Rename phpliteadmin.config.sample.php to phpliteadmin.config.php and change parameters in there.
+//    You can set only your custom settings in phpliteadmin.config.php. All other settings will be set to defaults.
+// 2. Change parameters directly in main phpliteadmin.php file
+//
+// Please see http://code.google.com/p/phpliteadmin/wiki/Configuration for more details
+
+//password to gain access
+$password = 'admin';
+
+//directory relative to this file to search for databases (if false, manually list databases in the $databases variable)
+$directory = '/tmp/';
+
+//whether or not to scan the subdirectories of the above directory infinitely deep
+$subdirectories = false;
+
+//if the above $directory variable is set to false, you must specify the databases manually in an array as the next variable
+//if any of the databases do not exist as they are referenced by their path, they will be created automatically
+$databases = array(
+	array(
+		'path'=> 'database1.sqlite',
+		'name'=> 'Database 1'
+	),
+	array(
+		'path'=> 'database2.sqlite',
+		'name'=> 'Database 2'
+	),
+);
+
+
+/* ---- Interface settings ---- */
+
+// Theme! If you want to change theme, save the CSS file in same folder of phpliteadmin or in folder "themes"
+$theme = 'phpliteadmin.css';
+
+// the default language! If you want to change it, save the language file in same folder of phpliteadmin or in folder "languages"
+// More about localizations (downloads, how to translate etc.): http://code.google.com/p/phpliteadmin/wiki/Localization
+$language = 'en';
+
+// set default number of rows. You need to relog after changing the number
+$rowsNum = 30;
+
+// reduce string characters by a number bigger than 10
+$charsNum = 300;
+
+
+/* ---- Custom functions ---- */
+
+//a list of custom functions that can be applied to columns in the databases
+//make sure to define every function below if it is not a core PHP function
+$custom_functions = array(
+	'md5', 'sha1', 'time', 'strtotime',
+	// add the names of your custom functions to this array
+	/* 'leet_text', */
+);
+
+// define your custom functions here
+/*
+function leet_text($value)
+{
+  return strtr($value, 'eaAsSOl', '344zZ01');
+}
+*/
+
+
+/* ---- Advanced options ---- */
+
+//changing the following variable allows multiple phpLiteAdmin installs to work under the same domain.
+$cookie_name = 'pla3412';
+
+//whether or not to put the app in debug mode where errors are outputted
+$debug = false;
+
+// the user is allowed to create databases with only these extensions
+$allowed_extensions = array('db','db3','sqlite','sqlite3');
