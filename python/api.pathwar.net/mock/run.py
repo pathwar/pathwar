0780b31816b7f156929e9175bfdedfa1e38904bf
Manfred Touron <m@42.am>
2015-01-03 14:22:35 +0100

    Mock: added models, attributes, initial content

--- python/api.pathwar.net/mock/run.py
+++ python/api.pathwar.net/mock/run.py
@@ -1,3 +1,25 @@
 from eve import Eve
+from settings import DOMAIN
 app = Eve()
-app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    with app.app_context():
+        # Drop everything
+        for collection in DOMAIN.keys():
+            app.data.driver.db[collection].drop()
+
+        organization_id = app.data.driver.db['organizations'].insert({
+            'name': 'example-organization',
+        })
+        user_id = app.data.driver.db['users'].insert({
+            'login': 'example-user',
+            'role': 'participant',
+        })
+        level_id = app.data.driver.db['levels'].insert({
+            'name': 'example-level',
+        })
+        hint_id = app.data.driver.db['hints'].insert({
+            'level_id': level_id,
+        })
+
+    app.run(debug=True, host='0.0.0.0')
--- python/api.pathwar.net/mock/settings.py
+++ python/api.pathwar.net/mock/settings.py
@@ -6,10 +6,24 @@ MONGO_HOST = os.environ['MONGO_PORT_27017_TCP_ADDR']
 MONGO_PORT = os.environ['MONGO_PORT_27017_TCP_PORT']
 
 
-achievements = {}
+XML = False
+
+
+achievements = {
+    'item_title': 'achievement',
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
+    'schema': {
+        'name': {
+            'type': 'string',
+            'unique': True,
+        },
+    },
+}
 
 
 coupons = {
+    'item_title': 'coupon',
     'resource_methods': ['GET', 'POST', 'DELETE'],
     'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'schema': {
@@ -23,17 +37,57 @@ coupons = {
 }
 
 
-items = {}
+items = {
+    'item_title': 'item',
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
+    'schema': {
+        'name': {
+            'type': 'string',
+            'unique': True,
+        },
+    },
+}
 
 
-levels = {}
+level_hints = {
+    'item_title': 'hint',
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
+    'url': 'levels/<regex(".*"):level_id>/hints',
+    'schema': {
+        'level_id': {
+            'type': 'string',
+        },
+    },
+}
 
 
-organizations = {
+levels = {
+    'item_title': 'level',
     'resource_methods': ['GET', 'POST', 'DELETE'],
     'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'additional_lookup': {
-        'url': 'regex("[\w]+")',
+        'url': 'regex(".*")',
+        'field': 'name',
+    },
+    'schema': {
+        'name': {
+            'type': 'string',
+            'minlength': 3,
+            'maxlength': 16,
+            'unique': True,
+        },
+    },
+}
+
+
+organizations = {
+    'item_title': 'organization',
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
+    'additional_lookup': {
+        'url': 'regex(".*")',
         'field': 'name',
     },
     'schema': {
@@ -50,21 +104,30 @@ organizations = {
 }
 
 
-sessions = {}
+sessions = {
+    'item_title': 'session',
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
+    'schema': {
+        'name': {
+            'type': 'string',
+            'unique': True,
+        },
+    },
+}
 
 
 users = {
+    'item_title': 'user',
     'resource_methods': ['GET', 'POST', 'DELETE'],
     'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'additional_lookup': {
-        'url': 'regex("[\w]+")',
+        'url': 'regex(".*")',
         'field': 'login',
     },
     'schema': {
         'login': {
             'type': 'string',
-            'minlength': 3,
-            'maxlength': 10,
             'unique': True,
         },
         'role': {
@@ -79,6 +142,7 @@ DOMAIN = {
     'achievements': achievements,
     'coupons': coupons,
     'items': items,
+    'level_hints': level_hints,
     'levels': levels,
     'organizations': organizations,
     'sessions': sessions,
