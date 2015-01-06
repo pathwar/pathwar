1583a5cf9afe6f9649cbdf7309f0340faad1d649
Manfred Touron <m@42.am>
2015-01-06 20:59:04 +0100

    Removed old comments

--- python/api.pathwar.net/mock/seeds.py
+++ python/api.pathwar.net/mock/seeds.py
@@ -36,56 +36,45 @@ def load_seeds(app, reset=True):
     root_id = app.data.driver.db['users'].insert({
         'login': 'root',
         'role': 'admin',
-        #'_id': str(uuid4()),
     })
     app.data.driver.db['user-tokens'].insert({
         'user': root_id,
         'token': 'root-token',
-        #'_id': str(uuid4()),
     })
 
     users = post(client, '/users', [{
         'login': 'joe',
         'email': 'joe@pathwar.net',
-        #'_id': str(uuid4()),
     }, {
         'login': 'm1ch3l',
         'email': 'm1ch3l@pathwar.net',
         'role': 'superuser',
-        #'_id': str(uuid4()),
     }])
 
     sessions = post(client, '/sessions', [{
         'name': 'new year super challenge',
-        #'_id': str(uuid4()),
     }, {
         'name': 'world battle',
-        #'_id': str(uuid4()),
     }])
 
     organizations = post(client, '/organizations', [{
         'name': 'pwn-around-the-world',
-        #'_id': str(uuid4()),
     }, {
         'name': 'staff',
-        #'_id': str(uuid4()),
     }])
 
     organizations_users = post(client, '/organization-users', [{
         'organization': organizations[0]['_items'][0]['_id'],
         'role': 'owner',
         'user': users[0]['_items'][0]['_id'],
-        #'_id': str(uuid4()),
     }, {
         'organization': organizations[0]['_items'][0]['_id'],
         'role': 'pwner',
         'user': users[0]['_items'][1]['_id'],
-        #'_id': str(uuid4()),
     }, {
         'organization': organizations[0]['_items'][1]['_id'],
         'role': 'owner',
         'user': str(root_id),
-        #'_id': str(uuid4()),
     }])
 
     levels = post(client, '/levels', [{
@@ -94,29 +83,24 @@ def load_seeds(app, reset=True):
         'price': 42,
         'tags': ['easy', 'welcome', 'official'],
         'author': 'Pathwar Team',
-        #'_id': str(uuid4()),
     }, {
         'name': 'pnu',
         'description': 'Possible not upload',
         'price': 420,
         'tags': ['php', 'advanced'],
         'author': 'Pathwar Team',
-        #'_id': str(uuid4()),
     }])
 
     level_hints = post(client, '/level-hints', [{
         'name': 'welcome sources',
         'price': 42,
         'level': levels[0]['_items'][0]['_id'],
-        #'_id': str(uuid4()),
     }, {
         'name': 'welcome full solution',
         'price': 420,
         'level': levels[0]['_items'][0]['_id'],
-        #'_id': str(uuid4()),
     }, {
         'name': 'pnu sources',
         'price': 42,
         'level': levels[0]['_items'][1]['_id'],
-        #'_id': str(uuid4()),
     }])
