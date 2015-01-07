26a63812509067e729135a1fd20dfb5b59b9f0a4
Manfred Touron <m@42.am>
2015-01-07 10:40:08 +0100

    Mock: Added organization-levels seeds

--- python/api.pathwar.net/mock/seeds.py
+++ python/api.pathwar.net/mock/seeds.py
@@ -131,3 +131,14 @@ def load_seeds(app, reset=True):
         'price': 42,
         'level': levels[0]['_items'][1]['_id'],
     }])
+
+    organization_levels = post(client, '/organization-levels', [{
+        'organization': organizations[0]['_items'][0]['_id'],
+        'level': levels[0]['_items'][0]['_id'],
+    }, {
+        'organization': organizations[0]['_items'][0]['_id'],
+        'level': levels[0]['_items'][1]['_id'],
+    }, {
+        'organization': organizations[0]['_items'][1]['_id'],
+        'level': levels[0]['_items'][1]['_id'],
+    }])
