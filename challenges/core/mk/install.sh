4543d4f9ab06796545572c292b32cbd415c7e16a
Manfred Touron <m@42.am>
2015-01-19 08:27:42 +0100

    Auto-ignoring pathwar-level.mk

--- challenges/core/mk/install.sh
+++ challenges/core/mk/install.sh
@@ -3,3 +3,4 @@
 BRANCH=${BRANCH:-master}
 
 wget https://raw.githubusercontent.com/pathwar/core/${BRANCH}/mk/pathwar-level.mk
+grep pathwar-level.mk .gitignore 2>/dev/null || echo pathwar-level.mk >> .gitignore
