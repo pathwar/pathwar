c3cabfacd59dae525e66b37dca3ff489f46e437c
Manfred Touron <m@42.am>
2015-01-23 09:35:18 +0100

    Added travis support in mk (Fixes #17)

--- challenges/core/mk/pathwar-level.mk
+++ challenges/core/mk/pathwar-level.mk
@@ -1,3 +1,11 @@
+SERVICES ?=	$(shell cat fig.yml | grep '^[a-z]' | cut -d: -f1)
+# By default $(SERVICE) is the first service in fig.yml
+SERVICE ?=	$(shell echo $(SERVICES) | tr " " "\n" | head -1)
+
+
+## Actions
+.PHONY: all build run
+
 all:	build
 
 build:
@@ -8,3 +16,20 @@ run:
 	fig up -d
 	fig ps
 	fig logs
+
+
+## Travis
+.PHONY: travis_install travis_run travis_run_service
+
+travis_install:
+	# Install travis-docker
+	curl -sLo - https://github.com/moul/travis-docker/raw/master/install.sh | sh -xe
+
+travis_run:
+	for service in $(SERVICES); do \
+	  $(MAKE) travis_run_service SERVICE=$$servie; \
+	done
+
+travis_run_service:
+	# ./run is a wrapper to allow travis to run docker commands
+	./run fig run $(SERVICE) /bin/bash -d 'echo OK'
