a362437cbfdf71bd71669af59aef94b75d92b0ce
Manfred Touron <m@42.am>
2015-01-27 21:26:19 +0100

    Handling post

--- tool/node-pathwar/lib/index.js
+++ tool/node-pathwar/lib/index.js
@@ -10,7 +10,7 @@ var Client = module.exports = function(config) {
 
 
 (function() {
-  this.get = function(path, options, cb) {
+  this.request = function(path, method, options, cb) {
     var url = config.api_endpoint + path.replace(/^\//, '');
     options = options || {};
     _.defaults(options, {
@@ -24,7 +24,24 @@ var Client = module.exports = function(config) {
     _.defaults(options.headers, {
       Authorization: 'Basic ' + new Buffer(config.token + ':').toString('base64')
     });
-    debug('GET ' + url, options);
-    return httpinvoke(url, 'GET', options, cb);
+    debug(method + ' ' + url, options);
+    return httpinvoke(url, method, options, cb);
+  };
+
+  this.get = function(path, options, cb) {
+    return this.request(path, 'GET', options, cb);
+  };
+
+  this.post = function(path, input, options, cb) {
+    options = options || {};
+    _.defaults(options, {
+      inputType: 'json',
+      headers: {},
+      input: input
+    });
+    _.defaults(options.headers, {
+      'Content-Type': 'application/json'
+    });
+    return this.request(path, 'POST', options, cb);
   };
 }).call(Client.prototype);
--- tool/node-pathwar/test/all.js
+++ tool/node-pathwar/test/all.js
@@ -17,28 +17,90 @@ describe("[client]", function() {
   });
 
   it("should successfully execute GET /", function(next) {
-    client.get("/")
-      .then(function(res) {
+    client.get("/").then(
+      function(res) {
         inspect('res', res);
         assert.equal(200, res.statusCode);
         next();
-      }, function(err) {
+      },
+      function(err) {
         inspect('err', err);
         assert();
       });
   });
 
   it("should successfully execute GET /levels", function(next) {
-    client.get("/levels")
-      .then(function(res) {
+    client.get("/levels").then(
+      function(res) {
         inspect('res', res);
         assert.equal(200, res.statusCode);
         assert.equal(true, res.body._items[0].price > 0);
         next();
-      }, function(err) {
+      },
+      function(err) {
         inspect('err', err);
         assert();
       });
   });
 
+  it("should successfully execute POST /user-tokens", function(next) {
+    client.post("/user-tokens", {
+      is_session: true
+    }).then(
+      function(res) {
+        inspect('res', res);
+        next();
+      },
+      function(err) {
+        inspect('err', err);
+        assert();
+      }
+    );
+  });
+
+  it("should chain a POST /user-tokens and a GET of the created resource using promise chaining", function(next) {
+    client.post("/user-tokens", {
+      is_session: true
+    }).then(
+      function(res) {
+        inspect('res', res);
+        return client.get(res.body._links.self.href);
+      },
+      function(err) {
+        inspect('err', err);
+        assert();
+      }
+    ).then(
+      function(res) {
+        inspect('res', res);
+        next();
+      },
+      function(err) {
+        inspect('err', err);
+        assert();
+      }
+    );
+  });
+
+  it("should chain a POST /user-tokens and a GET of the created resource using promise callback", function(next) {
+    client.post("/user-tokens", {
+      is_session: true
+    }).then(
+      function(res) {
+        inspect('res', res);
+        client.get(res.body._links.self.href).then(
+          function(res) {
+            inspect('res', res);
+            next();
+          },
+          function(err) {
+            inspect('err', err);
+            assert();
+          });
+      },
+      function(err) {
+        inspect('err', err);
+        assert();
+      });
+  });
 });
