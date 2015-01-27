b5aca29059afe966c0316b4a67b7c93995807e90
Manfred Touron <m@42.am>
2015-01-27 21:33:57 +0100

    Switched from BDD to TDD test interface

--- tool/node-pathwar/test/all.js
+++ tool/node-pathwar/test/all.js
@@ -1,106 +1,111 @@
 "use strict";
 
+
 var assert = require("assert"),
     debug = require("debug")("tests"),
     Client = require(".."),
     util = require('util');
 
+
 var inspect = function(name, obj) {
   debug(name, util.inspect(obj, {showHidden: false, depth: null}));
 };
 
-describe("[client]", function() {
+
+suite("[client]", function() {
   var client;
 
-  beforeEach(function() {
+  setup(function() {
     client = new Client();
   });
 
-  it("should successfully execute GET /", function(next) {
-    client.get("/").then(
-      function(res) {
-        inspect('res', res);
-        assert.equal(200, res.statusCode);
-        next();
-      },
-      function(err) {
-        inspect('err', err);
-        assert();
-      });
-  });
+  suite('#http-requests', function() {
+    test("should successfully execute GET /", function(next) {
+      client.get("/").then(
+        function(res) {
+          inspect('res', res);
+          assert.equal(200, res.statusCode);
+          next();
+        },
+        function(err) {
+          inspect('err', err);
+          assert();
+        });
+    });
 
-  it("should successfully execute GET /levels", function(next) {
-    client.get("/levels").then(
-      function(res) {
-        inspect('res', res);
-        assert.equal(200, res.statusCode);
-        assert.equal(true, res.body._items[0].price > 0);
-        next();
-      },
-      function(err) {
-        inspect('err', err);
-        assert();
-      });
-  });
+    test("should successfully execute GET /levels", function(next) {
+      client.get("/levels").then(
+        function(res) {
+          inspect('res', res);
+          assert.equal(200, res.statusCode);
+          assert.equal(true, res.body._items[0].price > 0);
+          next();
+        },
+        function(err) {
+          inspect('err', err);
+          assert();
+        });
+    });
 
-  it("should successfully execute POST /user-tokens", function(next) {
-    client.post("/user-tokens", {
-      is_session: true
-    }).then(
-      function(res) {
-        inspect('res', res);
-        next();
-      },
-      function(err) {
-        inspect('err', err);
-        assert();
-      }
-    );
-  });
+    test("should successfully execute POST /user-tokens", function(next) {
+      client.post("/user-tokens", {
+        is_session: true
+      }).then(
+        function(res) {
+          inspect('res', res);
+          next();
+        },
+        function(err) {
+          inspect('err', err);
+          assert();
+        }
+      );
+    });
 
-  it("should chain a POST /user-tokens and a GET of the created resource using promise chaining", function(next) {
-    client.post("/user-tokens", {
-      is_session: true
-    }).then(
-      function(res) {
-        inspect('res', res);
-        return client.get(res.body._links.self.href);
-      },
-      function(err) {
-        inspect('err', err);
-        assert();
-      }
-    ).then(
-      function(res) {
-        inspect('res', res);
-        next();
-      },
-      function(err) {
-        inspect('err', err);
-        assert();
-      }
-    );
-  });
+    test("should chain a POST /user-tokens and a GET of the created resource using promise chaining", function(next) {
+      client.post("/user-tokens", {
+        is_session: true
+      }).then(
+        function(res) {
+          inspect('res', res);
+          return client.get(res.body._links.self.href);
+        },
+        function(err) {
+          inspect('err', err);
+          assert();
+        }
+      ).then(
+        function(res) {
+          inspect('res', res);
+          next();
+        },
+        function(err) {
+          inspect('err', err);
+          assert();
+        }
+      );
+    });
 
-  it("should chain a POST /user-tokens and a GET of the created resource using promise callback", function(next) {
-    client.post("/user-tokens", {
-      is_session: true
-    }).then(
-      function(res) {
-        inspect('res', res);
-        client.get(res.body._links.self.href).then(
-          function(res) {
-            inspect('res', res);
-            next();
-          },
-          function(err) {
-            inspect('err', err);
-            assert();
-          });
-      },
-      function(err) {
-        inspect('err', err);
-        assert();
-      });
+    test("should chain a POST /user-tokens and a GET of the created resource using promise callback", function(next) {
+      client.post("/user-tokens", {
+        is_session: true
+      }).then(
+        function(res) {
+          inspect('res', res);
+          client.get(res.body._links.self.href).then(
+            function(res) {
+              inspect('res', res);
+              next();
+            },
+            function(err) {
+              inspect('err', err);
+              assert();
+            });
+        },
+        function(err) {
+          inspect('err', err);
+          assert();
+        });
+    });
   });
 });
