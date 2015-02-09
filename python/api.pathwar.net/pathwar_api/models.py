1ce12842833e0a0b05781aac2348b5a8877ac403
Manfred Touron <m@42.am>
2015-02-09 08:33:08 +0100

    Reworked fields permissions on user-activities

--- python/api.pathwar.net/pathwar_api/models.py
+++ python/api.pathwar.net/pathwar_api/models.py
@@ -715,10 +715,18 @@ user_activities = {
     # FIXME: INTERNAL
     'item_title': 'user activitie',
     'resource_title': 'user activities',
+
+    # collection
     'resource_methods': ['GET', 'POST', 'DELETE'],
-    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'public_methods': [],
+    'allowed_item_read_roles': ['user', 'moderator', 'admin'],
+    'allowed_item_write_roles': ['admin'],
+    # item
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'public_item_methods': [],
+    'allowed_item_read_roles': ['user', 'moderator', 'admin'],
+    'allowed_item_write_roles': ['admin'],
+
     # 'url': 'users/<user>/activities',
     'schema': {
         'user': {
