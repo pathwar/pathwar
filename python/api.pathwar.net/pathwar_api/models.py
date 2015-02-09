1d230af57308f949293dab105157ac64f58cdb53
Manfred Touron <m@42.am>
2015-02-09 08:27:01 +0100

    Reworked fields permissions on organization-achievements

--- python/api.pathwar.net/pathwar_api/models.py
+++ python/api.pathwar.net/pathwar_api/models.py
@@ -304,10 +304,18 @@ levels = {
 organization_achievements = {
     'item_title': 'organization earned achievement',
     'resource_title': 'organization earned achievements',
+
+    # collection
     'resource_methods': ['GET', 'POST', 'DELETE'],
-    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'public_methods': [],
+    'allowed_item_read_roles': ['user', 'moderator', 'admin'],
+    'allowed_item_write_roles': ['user', 'admin'],
+    # item
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'public_item_methods': [],
+    'allowed_item_read_roles': ['user', 'moderator', 'admin'],
+    'allowed_item_write_roles': ['user', 'admin'],
+
     # 'url': 'organizations/<organization>/achievements',
     'schema': {
         'organization': {
