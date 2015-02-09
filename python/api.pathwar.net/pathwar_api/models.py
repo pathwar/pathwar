6bae662cf006f8acf5ea9eb60467ff1c3799e1b7
Manfred Touron <m@42.am>
2015-02-09 08:30:59 +0100

    Reworked fields permissions on organization-items

--- python/api.pathwar.net/pathwar_api/models.py
+++ python/api.pathwar.net/pathwar_api/models.py
@@ -480,10 +480,18 @@ organization_levels = {
 organization_items = {
     'item_title': 'organization item',
     'resource_title': 'organization items',
+
+    # collection
     'resource_methods': ['GET', 'POST', 'DELETE'],
-    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'public_methods': [],
+    'allowed_item_read_roles': ['user', 'moderator', 'admin'],
+    'allowed_item_write_roles': ['user', 'admin'],
+    # item
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
     'public_item_methods': [],
+    'allowed_item_read_roles': ['user', 'moderator', 'admin'],
+    'allowed_item_write_roles': ['user', 'admin'],
+
     # 'url': 'organizations/<organization>/items',
     'schema': {
         'organization': {
