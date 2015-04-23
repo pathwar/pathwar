e704336fdf9d6173494d0b4520c51766f1c90c93
s. rannou <mxs@sbrk.org>
2015-04-23 23:05:56 +0200

    Added publish_on_s3. #22

--- challenges/core/mk/pathwar-level.mk
+++ challenges/core/mk/pathwar-level.mk
@@ -1,6 +1,7 @@
 SERVICES ?=	$(shell cat docker-compose.yml | grep '^[a-z]' | cut -d: -f1)
 # By default $(SERVICE) is the first service in docker-compose.yml
 SERVICE ?=	$(shell echo $(SERVICES) | tr " " "\n" | head -1)
+S3_URL ?= 	s3://pathwar-levels/
 
 PACKAGE_NAME = package-$(shell basename $(shell pwd)).tar
 
@@ -33,6 +34,8 @@ $(PACKAGE_NAME): package-level
 	-rm -f $(PACKAGE_NAME)
 	./package-level build
 
+publish_on_s3:
+	s3cmd put $(PACKAGE_NAME) $(S3_URL)/$(head -c 10 /dev/urandom | md5sum | awk '// { print $1; }').tar
 
 ## Travis
 .PHONY: travis_install travis_run travis_run_service
