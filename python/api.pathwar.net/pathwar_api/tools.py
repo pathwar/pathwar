5cf5fa39436e83f2154bcdcb21ba6de66e5c3915
Manfred Touron <m@42.am>
2015-04-29 23:35:46 +0200

    Fixed email validation

--- python/api.pathwar.net/pathwar_api/tools.py
+++ python/api.pathwar.net/pathwar_api/tools.py
@@ -6,13 +6,13 @@ bp_tools = Blueprint('tools', __name__)
 @bp_tools.route('/email-verify/<string:user_id>/'
                 '<string:email_verification_token>')
 def email_verify(user_id, email_verification_token):
-    user = current_app.data.driver.db['users'].find_one({
+    user = current_app.data.driver.db['raw-users'].find_one({
         '_id': str(user_id),
         'active': False,
         'email_verification_token': str(email_verification_token),
     })
     if user:
-        current_app.data.driver.db['users'].update(
+        current_app.data.driver.db['raw-users'].update(
             {
                 '_id': str(user_id)
             }, {
@@ -22,8 +22,15 @@ def email_verify(user_id, email_verification_token):
                 },
             },
         )
-        # FIXME: 302 redirect to the portal
-        return 'Email validated (FIXME: 302)'
+        return """<html>
+  <head>
+    <meta http-equiv="refresh" content="10; url=http://portal.pathwar.net/" />
+  </head>
+  <body>
+    <p>Email validated, <a href="http://portal.pathwar.net/">go to portal</a>.</p>
+  </body>
+</html>"""
+
     else:
         abort(404)
     print(user, user_id, email_verification_token)
