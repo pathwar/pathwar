8dca72033bd799828ba6d7a03d3ff935812ddaf3
Manfred Touron <m@42.am>
2015-09-14 22:35:02 +0200

    Using dynamic protocal assets URIs

--- level/web/training-sqli/www/header.php
+++ level/web/training-sqli/www/header.php
@@ -2,5 +2,5 @@
 <html lang="en">
 <head>
   <title>Training Include</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 </head>
