7ac4b2c0fcf71e77b1ac55d80387dc28407c1c4d
Manfred Touron <m@42.am>
2015-10-12 22:25:26 +0200

    Parsing is in cmd/*

--- tool/go-pathwar/cmd/pathwar-cli/main.go
+++ tool/go-pathwar/cmd/pathwar-cli/main.go
@@ -4,13 +4,13 @@ import (
 	"os"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/pathwar/go-pathwar/pkg/pathwar"
+	"github.com/pathwar/go-pathwar/pkg/api"
 )
 
 func main() {
-	ec, err := pathwar.Start(os.Args)
+	cli := api.NewAPIPathwar(os.Getenv("PATHWAR_TOKEN"), os.Getenv("PATHWAR_DEBUG"))
+	_, err := cli.PostRequest("organization-coupons", map[string]string{"organization": "283e4414-34cc-472c-91b5-4b7ed0cf8d92", "coupon": "poeut"})
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	os.Exit(ec)
 }
