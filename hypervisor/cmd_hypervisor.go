40d113acfc2749ee419997b17dc8241a56fd48ef
Manfred Touron <m@42.am>
2019-02-07 20:39:46 +0100

    feat: initial docker hypervisor

---
+++ hypervisor/cmd_hypervisor.go
@@ -0,0 +1,43 @@
+package hypervisor
+
+import (
+	"encoding/json"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+
+	"pathwar.pw/pkg/cli"
+)
+
+type Options struct {
+}
+
+func (opts Options) String() string {
+	out, _ := json.Marshal(opts)
+	return string(out)
+}
+
+func Commands() cli.Commands {
+	return cli.Commands{
+		"hypervisor":     &hypervisorCommand{},
+		"hypervisor run": &runCommand{},
+	}
+}
+
+type hypervisorCommand struct{ opts Options }
+
+func (cmd *hypervisorCommand) LoadDefaultOptions() error { return viper.Unmarshal(&cmd.opts) }
+func (cmd *hypervisorCommand) ParseFlags(flags *pflag.FlagSet) {
+	if err := viper.BindPFlags(flags); err != nil {
+		zap.L().Warn("failed to bind viper flags", zap.Error(err))
+	}
+}
+func (cmd *hypervisorCommand) CobraCommand(commands cli.Commands) *cobra.Command {
+	command := &cobra.Command{
+		Use: "hypervisor",
+	}
+	command.AddCommand(commands["hypervisor run"].CobraCommand(commands))
+	return command
+}
