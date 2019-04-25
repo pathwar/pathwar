b0e7f90b5312aa1d4a95b5617ca93329402bf083
Manfred Touron <m@42.am>
2019-04-25 14:46:53 +0200

    feat: add GET /users

--- server/api.pub.tournaments.go
+++ server/api.pub.tournaments.go
@@ -6,8 +6,8 @@ import (
 	"pathwar.pw/entity"
 )
 
-func (s *svc) Tournaments(ctx context.Context, _ *Void) (*entity.Tournaments, error) {
-	var tournaments entity.Tournaments
+func (s *svc) Tournaments(ctx context.Context, _ *Void) (*entity.TournamentList, error) {
+	var tournaments entity.TournamentList
 	if err := s.db.Find(&tournaments.Items).Error; err != nil {
 		return nil, err
 	}
