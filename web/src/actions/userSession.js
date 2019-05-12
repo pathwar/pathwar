9223f353b8769821f2b2920c43e56f28edeb26c1
Lucas Germano <lucasgdev@gmail.com>
2019-05-12 00:58:17 -0300

    Set structure to save user token as cookie

--- web/src/actions/userSession.js
+++ web/src/actions/userSession.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
+import Cookies from "js-cookie";
 import { 
 	PERFORM_LOGIN, 
 	LOGIN_FAILED,
 	SET_USER_SESSION
 } from "../constants/actionTypes"
+import { USER_SESSION_TOKEN_NAME } from "../constants/userSession";
 import { history } from "../store/configureStore";
 import { performLogin } from "../api/userSession"
 
@@ -18,6 +21,8 @@ export const performLoginAction = (email, password) => async dispatch => {
 			type: SET_USER_SESSION,
 			payload: { activeUser: response.data }
 		});
+
+		// Cookies.set(USER_SESSION_TOKEN_NAME, response.data.token)
 
 		history.push("/dashboard");
 	} catch (error) {
