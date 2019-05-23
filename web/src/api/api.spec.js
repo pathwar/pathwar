0179199f1047d9746eb72626ee1425f185fb4ee3
Manfred Touron <m@42.am>
2019-05-23 17:51:42 +0200

    test: testing login

--- web/src/api/api.spec.js
+++ web/src/api/api.spec.js
@@ -8,5 +8,38 @@ describe('API', () => {
                 .set('Accept', 'application/json')
                 .expect(200);
         });
+        it('should return a 501 response', () => {
+            return api.post('/api/ping')
+                .set('Accept', 'application/json')
+                .expect(501);
+        });
+    });
+    var token = '';
+    describe('login flow', () => {
+        it('should return a token', () => {
+            return api.post('/api/authenticate')
+                .send({'username': 'integration'})
+                .expect(200)
+                .expect(response => {
+                    if (!('token' in response.body)) {
+                        throw new Error('missing token');
+                    }
+                })
+                .then(response => {
+                    token = response.body.token;
+                })
+        });
+        it('should return a 401 response without token', () => {
+            return api.get('/api/user-session')
+                .expect(401);
+        });
+        it('should return a 200 response with a valid token', () => {
+            return api.get('/api/user-session')
+                .set('Authorization', token)
+                .expect(200, {
+                    'metadata': {},
+                    'username': 'integration',
+                });
+        });
     });
 });
