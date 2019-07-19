fcb5b3f75e4b121932173cdebea8cdd02aba330b
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2019-07-19 19:28:07 +0700

    feat: Add Coming soon page

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `Pathwar.land`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,3 +1,38 @@
 import React from "react"
+import {Helmet} from "react-helmet"
+import Img from "gatsby-image"
 
-export default () => <div>Hello world!</div>
+import { graphql } from "gatsby";
+
+import styles from "./index.module.css";
+
+const title = "Pathwar.land";
+const description = "Pathwar is an educational platform with a focus on security/cryptography, where you can go through levels (courses) to learn new things and hone your skills.";
+
+export default ({ data }) => <div className={styles.page}>
+  <Helmet defer={false}>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+  </Helmet>
+
+  <div className={styles.content}>
+    <div>
+      <Img fixed={data.file.childImageSharp.fixed} alt="Pathwar Logo" />
+    </div>
+    <h1>Pathwar Land</h1>
+    <p>{description}</p>
+    <h2 className={styles.comingSoon}>Coming Soon..</h2>
+  </div>
+</div>
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "images/pathwar-logo.png" }) {
+      childImageSharp {
+        fixed(width: 200, height: 200) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
