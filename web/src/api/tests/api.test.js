707e3e21f9bbfcdcc9a08b799d2f89eac7cad044
Manfred Touron <m@42.am>
2019-11-05 12:30:23 +0100

    chore: port JS test to new API

--- web/src/api/tests/api.test.js
+++ web/src/api/tests/api.test.js
@@ -16,7 +16,7 @@ unsafeApi.interceptors.request.use(withToken);
 //Test session variables
 let active_season_id = undefined
 let team_id = undefined
-let challenge_id = undefined
+let season_challenge_id = undefined
 
 //Helpers
 const performUserSessionCalls = async () => {
@@ -32,9 +32,9 @@ const performUserSessionCalls = async () => {
 
   //Set a real challenge_id from the season for tests
   try {
-    const challengesResponse = await unsafeApi.get("/challenges");
-    const firstItem = challengesResponse.data.items[0]
-    challenge_id = firstItem.id
+    const seasonChallengesResponse = await unsafeApi.get(`/season-challenges?season_id=${active_season_id}`);
+    const firstItem = seasonChallengesResponse.data.items[0]
+    season_challenge_id = firstItem.id
   } catch (error) {
     throw error;
   }
@@ -55,46 +55,37 @@ beforeAll(() => {
   return performUserSessionCalls();
 })
 
-describe('User Session API Calls', () => {
+describe('API Calls', () => {
   it('should work GET user session - /user/session', async () => {
     const response = await unsafeApi.get("/user/session");
     expect(response.status).toEqual(200);
     expect(response.data).toBeDefined();
   })
-})
-
-describe('Organizations API Calls', () => {
   it('should work GET all organizations - /organizations', async () => {
     const response = await unsafeApi.get("/organizations");
     expect(response.status).toEqual(200);
     expect(response.data).toBeDefined();
   })
-})
-
-describe('Seasons API Calls', () => {
   it('should work POST preferences - /preferences', async () => {
     const preferencesPost = await unsafeApi.post(`/user/preferences`, {"active_season_id": active_season_id});
     expect(preferencesPost.status).toEqual(200);
   })
-
   it('should work GET all teams on a season - /teams?season_id=the_id', async () => {
     const getAllTeamsResponse = await unsafeApi.get(`/teams?season_id=${active_season_id}`);
     expect(getAllTeamsResponse.status).toEqual(200);
   })
-  it('should work GET all challenges on a season - /challenges', async () => {
-    const response = await unsafeApi.get("/challenges");
+  it('should work GET all challenges on a season - /season-challenges', async () => {
+    const response = await unsafeApi.get(`/season-challenges?season_id=${active_season_id}`);
     expect(response.status).toEqual(200);
     expect(response.data).toBeDefined();
   })
-  it('should work GET challenge details - /challenge?challenge_id=the_id', async () => {
-    const response = await unsafeApi.get(`/challenge?challenge_id=${challenge_id}`);
-
+  it('should work GET season challenge details - /season-challenge?season_challenge_id=the_id', async () => {
+    const response = await unsafeApi.get(`/season-challenge?season_challenge_id=${season_challenge_id}`);
     expect(response.status).toEqual(200);
     expect(response.data).toBeDefined();
   })
   it('should work GET team details - /team?team_id=the_id', async () => {
     const response = await unsafeApi.get(`/team?team_id=${team_id}`);
-
     expect(response.status).toEqual(200);
     expect(response.data).toBeDefined();
   })
