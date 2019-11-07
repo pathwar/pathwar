4a39c7dae456756e650634673d257b8c3b251c18
Manfred Touron <m@42.am>
2019-11-07 18:59:36 +0100

    feat: 'pathwar compose'

---
+++ go/pkg/pwcompose/compose.go
@@ -0,0 +1,23 @@
+package pwcompose
+
+import "fmt"
+
+func Bundle( /* opts */ ) error {
+	return fmt.Errorf("not implemented")
+}
+
+func Up( /* opts */ ) error {
+	return fmt.Errorf("not implemented")
+}
+
+func Logs( /* opts */ ) error {
+	return fmt.Errorf("not implemented")
+}
+
+func Down( /* opts */ ) error {
+	return fmt.Errorf("not implemented")
+}
+
+func PS( /* opts */ ) error {
+	return fmt.Errorf("not implemented")
+}
