480c2743da464f9c5f4d22ad8440372b774d6d19
Lucas Germano <lucasgdev@gmail.com>
2019-11-21 02:37:39 -0300

    Create submit team form, create stamp behaviour

--- web/src/components/team/CreateTeamStampCard.js
+++ web/src/components/team/CreateTeamStampCard.js
@@ -1,20 +1,65 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
-import { StampCard } from "tabler-react";
+import { isEmpty } from "ramda";
+import { StampCard, Form, Button } from "tabler-react";
 
-const CreateTeamStampCard = () => {
+const CreateTeamStampCard = ({ activeSeason, createTeam }) => {
+  const [isFormOpen, setFormOpen] = useState(false)
+  const [name, setName] = useState("")
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if (!isEmpty(name)) {
+      setError(false)
+    }
+  }, [name])
+
+  const handleChange = (event) => setName(event.target.value)
+  const handleFormOpen = (event) => {
+    event.preventDefault();
+    setFormOpen(!isFormOpen);
+  }
+
+
+  const submitTeamCreate = async (event) => {
+    event.preventDefault();
+    if (isEmpty(name)) {
+      setError(true)
+      return;
+    } else {
+      await createTeam(activeSeason.id, name);
+      setFormOpen(false)
+    }
+  }
 
     return (
+      <>
         <StampCard
         color="blue"
         icon="users"
         header={
-          <Link to="/validate-coupon">
+          <Link to="/" onClick={handleFormOpen}>
             <small>Create new team</small>
           </Link>
         }
         footer={"Ahoy! Create a new team"}
       />
+      {isFormOpen && (
+        <form onSubmit={submitTeamCreate}>
+        <Form.FieldSet>
+          <Form.Group isRequired label="Name">
+            <Form.Input name="name" onChange={handleChange} invalid={error} cross={error} feedback={error && "Please, insert a name"} />
+          </Form.Group>
+          <Form.Group>
+            <Button type="submit" color="primary" className="ml-auto">
+              Send
+            </Button>
+          </Form.Group>
+        </Form.FieldSet>
+      </form>
+
+      )}
+      </>
     )
 }
 
--- web/src/pages/SeasonPage.js
+++ web/src/pages/SeasonPage.js
@@ -1,74 +1,91 @@
-import * as React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { Page, Grid } from "tabler-react";
-import { isNil } from "ramda";
+import * as React from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { Page, Grid } from "tabler-react"
+import { isNil } from "ramda"
 
 import AllTeamsOnSeasonList from "../components/season/AllTeamsOnSeasonList"
-import ChallengesCardPreview from "../components/challenges/ChallengeCardPreview";
-import ValidationCouponStampCard from "../components/coupon/ValidateCouponStampCard";
-import CreateTeamStampCard from "../components/team/CreateTeamStampCard";
+import ChallengesCardPreview from "../components/challenges/ChallengeCardPreview"
+import ValidationCouponStampCard from "../components/coupon/ValidateCouponStampCard"
+import CreateTeamStampCard from "../components/team/CreateTeamStampCard"
 
 import {
-  fetchChallenges as fetchChallengesAction ,
+  fetchChallenges as fetchChallengesAction,
   fetchAllSeasonTeams as fetchAllSeasonTeamsAction,
-  buyChallenge as buyChallengeAction
-} from "../actions/seasons";
+  buyChallenge as buyChallengeAction,
+  createTeam as createTeamAction,
+} from "../actions/seasons"
 
 class SeasonPage extends React.Component {
+  componentDidUpdate(prevProps) {
+    const {
+      fetchAllSeasonTeamsAction,
+      fetchChallengesAction,
+      seasons: { activeSeason },
+    } = this.props
+    const {
+      seasons: { activeSeason: prevActiveSeason },
+    } = prevProps
 
-    componentDidUpdate(prevProps) {
-      const { fetchAllSeasonTeamsAction, fetchChallengesAction, seasons: { activeSeason } } = this.props;
-      const { seasons: { activeSeason: prevActiveSeason } } = prevProps;
-
-      if (isNil(prevActiveSeason) && activeSeason ) {
-        fetchAllSeasonTeamsAction(activeSeason.id);
-        fetchChallengesAction(activeSeason.id);
-      }
+    if (isNil(prevActiveSeason) && activeSeason) {
+      fetchAllSeasonTeamsAction(activeSeason.id)
+      fetchChallengesAction(activeSeason.id)
     }
+  }
 
-    render() {
-        const { buyChallengeAction, seasons: { activeSeason, activeChallenges, allTeamsOnSeason } } = this.props;
-        const name = activeSeason ? activeSeason.name : undefined;
+  render() {
+    const {
+      buyChallengeAction,
+      createTeamAction,
+      seasons: { activeSeason, activeChallenges, allTeamsOnSeason },
+    } = this.props
+    const name = activeSeason ? activeSeason.name : undefined
 
-        return (
-            <Page.Content title="Season" subTitle={name}>
-                <Grid.Row>
-                  <Grid.Col xs={12} sm={3} lg={3}>
-                  <h3>Actions</h3>
-                    <ValidationCouponStampCard />
-                    <CreateTeamStampCard />
-                    <h3>Teams</h3>
-                    <AllTeamsOnSeasonList activeSeason={activeSeason} allTeamsOnSeason={allTeamsOnSeason} />
-                  </Grid.Col>
-                  <Grid.Col xs={12} sm={9} lg={9}>
-                    <h3>Challenges</h3>
-                    <ChallengesCardPreview challenges={activeChallenges} buyChallenge={buyChallengeAction} />
-                  </Grid.Col>
-
-                </Grid.Row>
-              </Page.Content>
-          );
-    }
+    return (
+      <Page.Content title="Season" subTitle={name}>
+        <Grid.Row>
+          <Grid.Col xs={12} sm={3} lg={3}>
+            <h3>Actions</h3>
+            <ValidationCouponStampCard />
+            <CreateTeamStampCard
+              activeSeason={activeSeason}
+              createTeam={createTeamAction}
+            />
+            <h3>Teams</h3>
+            <AllTeamsOnSeasonList
+              activeSeason={activeSeason}
+              allTeamsOnSeason={allTeamsOnSeason}
+            />
+          </Grid.Col>
+          <Grid.Col xs={12} sm={9} lg={9}>
+            <h3>Challenges</h3>
+            <ChallengesCardPreview
+              challenges={activeChallenges}
+              buyChallenge={buyChallengeAction}
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Page.Content>
+    )
+  }
 }
 
 SeasonPage.propTypes = {
-    seasons: PropTypes.object,
-    fetchChallengesAction: PropTypes.func
-};
+  seasons: PropTypes.object,
+  fetchChallengesAction: PropTypes.func,
+}
 
 const mapStateToProps = state => ({
-    seasons: state.seasons,
-    activeOrganization: state.organizations.activeOrganization
-});
+  seasons: state.seasons,
+  activeOrganization: state.organizations.activeOrganization,
+})
 
 const mapDispatchToProps = {
-  fetchChallengesAction: (seasonID) => fetchChallengesAction(seasonID),
-  fetchAllSeasonTeamsAction: (seasonID) => fetchAllSeasonTeamsAction(seasonID),
-  buyChallengeAction: (seasonID, teamID) => buyChallengeAction(seasonID, teamID)
-};
+  fetchChallengesAction: seasonID => fetchChallengesAction(seasonID),
+  fetchAllSeasonTeamsAction: seasonID => fetchAllSeasonTeamsAction(seasonID),
+  buyChallengeAction: (seasonID, teamID) =>
+    buyChallengeAction(seasonID, teamID),
+  createTeamAction: (seasonID, name) => createTeamAction(seasonID, name),
+}
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SeasonPage);
+export default connect(mapStateToProps, mapDispatchToProps)(SeasonPage)
