c8eb8227b5d8b5aa3ef7cc510afbc8799e5780fc
zaraki <zaraki@osmose.world>
2020-02-19 18:20:11 +0100

    feat: forestadmin lumber init

---
+++ tool/forestadmin/routes/challenge-instance.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { challenge_instance } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('challenge_instance');
+
+// This file contains the logic of every route in Forest Admin for the collection challenge_instance:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Challenge Instance
+router.post('/challenge_instance', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Challenge Instance
+router.put('/challenge_instance/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Challenge Instance
+router.delete('/challenge_instance/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Challenge Instances
+router.get('/challenge_instance', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Challenge Instances
+router.get('/challenge_instance/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Challenge Instance
+router.get('/challenge_instance/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Challenge Instances
+router.get('/challenge_instance.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+module.exports = router;
---
+++ tool/forestadmin/routes/whoswho-attempt.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { whoswho_attempt } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('whoswho_attempt');
+
+// This file contains the logic of every route in Forest Admin for the collection whoswho_attempt:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Whoswho Attempt
+router.post('/whoswho_attempt', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Whoswho Attempt
+router.put('/whoswho_attempt/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Whoswho Attempt
+router.delete('/whoswho_attempt/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Whoswho Attempts
+router.get('/whoswho_attempt', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Whoswho Attempts
+router.get('/whoswho_attempt/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Whoswho Attempt
+router.get('/whoswho_attempt/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Whoswho Attempts
+router.get('/whoswho_attempt.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+module.exports = router;
