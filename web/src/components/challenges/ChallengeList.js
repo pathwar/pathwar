cc2c19522ab0c4d7e9dda7d204d23899351e33b2
Lucas Germano <lucasgdev@gmail.com>
2020-05-15 11:36:33 -0300

    Adjust card actions inside new card

--- web/src/components/challenges/ChallengeList.js
+++ web/src/components/challenges/ChallengeList.js
@@ -3,131 +3,78 @@ import React from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { Card, Button, Dimmer, Table, Progress } from "tabler-react"
+import {
+  Button,
+  Dimmer,
+  ProgressCard,
+  Grid,
+} from "tabler-react"
 import styles from "../../styles/layout/loader.module.css"
 
-import {
-  buyChallenge as buyChallengeAction
-} from "../../actions/seasons"
+import { buyChallenge as buyChallengeAction } from "../../actions/seasons"
 
-const ChallengeRow = ({
-  challenge,
-  teamId,
-  buyChallenge
-}) => {
-  const { flavor } = challenge
-  const hasSubscriptions = challenge.subscriptions
+const ChallengeCard = (challenge, buyChallenge) => {
+  const {
+    challenge: { flavor, subscriptions, id: challengeID },
+    teamId,
+  } = challenge
 
   return (
-    <>
-      <Table.Row>
-        <Table.Col>
-          <strong>{flavor.challenge.name}</strong>
-        </Table.Col>
-        <Table.Col className="text-nowrap">{flavor.challenge.author}</Table.Col>
-        <Table.Col>
-          <div className="clearfix">
-            <div className="float-left">
-              <strong>42%</strong>
-            </div>
-          </div>
-          <Progress size="sm">
-            <Progress.Bar color="yellow" width={42} />
-          </Progress>
-        </Table.Col>
-        <Table.Col className="w-1">
+    <ProgressCard
+      header={flavor.challenge.name}
+      content={
+        <Button.List>
           <Button
             RootComponent={Link}
-            to={`/app/challenge/${challenge.id}`}
+            to={`/app/challenge/${challengeID}`}
+            target="_blank"
             color="info"
             size="sm"
             icon="eye"
-          />
-        </Table.Col>
-        <Table.Col className="w-1">
+          >
+            Open
+          </Button>
           <Button
-            onClick={() => buyChallenge(challenge.id, teamId)}
-            value="Buy"
+            onClick={() => buyChallenge(challengeID, teamId)}
             size="sm"
             color="success"
-            disabled={hasSubscriptions}
-            icon={hasSubscriptions ? "check" : "dollar-sign"}
-          />
-        </Table.Col>
-        <Table.Col className="w-1">
-          <Button value="Github page" social="github" size="sm" />
-        </Table.Col>
-      </Table.Row>
-    </>
-  )
-}
-
-const ChallengeTable = ({
-  challenges,
-  teamId,
-  seasonId,
-  buyChallenge
-}) => {
-  return (
-    <Table
-      cards={true}
-      striped={true}
-      responsive={true}
-      className="table-vcenter"
-    >
-      <Table.Header>
-        <Table.Row>
-          <Table.ColHeader>Flavor</Table.ColHeader>
-          <Table.ColHeader>Author</Table.ColHeader>
-          <Table.ColHeader>Progress</Table.ColHeader>
-          <Table.ColHeader>View</Table.ColHeader>
-          <Table.ColHeader>Buy</Table.ColHeader>
-          <Table.ColHeader>Page</Table.ColHeader>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {challenges.map((challenge, idx) => {
-          return (
-            <ChallengeRow
-              key={Math.random() + idx}
-              challenge={challenge}
-              buyChallenge={buyChallenge}
-              teamId={teamId}
-              seasonId={seasonId}
-            />
-          )
-        })}
-      </Table.Body>
-    </Table>
+            disabled={subscriptions}
+            icon={subscriptions ? "check" : "dollar-sign"}
+          >
+            Buy
+          </Button>
+        </Button.List>
+      }
+      progressColor="green"
+      progressWidth={84}
+    />
   )
 }
 
 const ChallengeList = props => {
-  const {
-    challenges,
-    activeUserSession,
-    buyChallengeAction
-  } = props
-  const { active_team_member, active_season_id } =
-    (activeUserSession && activeUserSession.user) || {}
+  const { challenges, activeUserSession, buyChallengeAction } = props
 
   return !challenges || !activeUserSession ? (
     <Dimmer className={styles.dimmer} active loader />
   ) : (
-    <Card>
-      <ChallengeTable
-        challenges={challenges}
-        buyChallenge={buyChallengeAction}
-        teamId={active_team_member.team_id}
-        seasonId={active_season_id}
-      />
-    </Card>
+    <>
+      <Grid.Row>
+        {challenges.map(challenge => (
+          <Grid.Col lg={4} sm={4} md={4} xs={4}>
+            <ChallengeCard
+              challenge={challenge}
+              buyChallenge={buyChallengeAction}
+            />
+          </Grid.Col>
+        ))}
+      </Grid.Row>
+    </>
   )
 }
 
 ChallengeList.propTypes = {
   buyChallengeAction: PropTypes.func,
-  activeTeamId: PropTypes.string
+  activeTeamId: PropTypes.string,
 }
 
 const mapStateToProps = state => ({
@@ -136,10 +83,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {
   buyChallengeAction: (challengeID, teamID, seasonId) =>
-    buyChallengeAction(challengeID, teamID, seasonId)
+    buyChallengeAction(challengeID, teamID, seasonId),
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ChallengeList)
+export default connect(mapStateToProps, mapDispatchToProps)(ChallengeList)
