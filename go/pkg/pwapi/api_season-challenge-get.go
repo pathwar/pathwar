bbf85561a072719c778b8c58472a126098a30cfb
zaraki <zaraki@osmose.world>
2020-05-20 17:21:52 +0200

    feat: GET /season-challenge now returns instances nginx_url

--- go/pkg/pwapi/api_season-challenge-get.go
+++ go/pkg/pwapi/api_season-challenge-get.go
@@ -2,6 +2,7 @@ package pwapi
 
 import (
 	"context"
+	"fmt"
 
 	"pathwar.land/v2/go/pkg/errcode"
 	"pathwar.land/v2/go/pkg/pwdb"
@@ -33,12 +34,25 @@ func (svc *service) SeasonChallengeGet(ctx context.Context, in *SeasonChallengeG
 		Preload("Season").
 		Preload("Flavor").
 		Preload("Flavor.Challenge").
+		Preload("Flavor.Instances").
+		Preload("Flavor.Instances.Agent"). // FIXME: where status==active
 		Preload("Subscriptions", "team_id = ?", team.ID).
 		Preload("Subscriptions.Validations").
 		First(&item).
 		Error
 	if err != nil {
 		return nil, errcode.ErrGetSeasonChallenge.Wrap(err)
+	}
+	for _, instance := range item.Flavor.Instances {
+		// FIXME: hide instances without nginx-url?
+		if instance.Agent != nil {
+			hash, err := pwdb.ChallengeInstancePrefixHash(fmt.Sprintf("%d", instance.ID), userID, instance.Agent.AuthSalt)
+			if err != nil {
+				return nil, errcode.ErrGeneratePrefixHash.Wrap(err)
+			}
+			instance.NginxURL = fmt.Sprintf("http://%s.%s", hash, instance.Agent.DomainSuffix)
+			instance.Agent = nil
+		}
 	}
 
 	ret := SeasonChallengeGet_Output{Item: &item}
