c019d8616a24b3af0041a988d43a1f9f1ea86191
zaraki <zaraki@osmose.world>
2020-05-27 17:25:08 +0200

    feat: challenge validation now works properly

--- go/pkg/pwapi/api_challenge-subscription-validate.go
+++ go/pkg/pwapi/api_challenge-subscription-validate.go
@@ -2,14 +2,16 @@ package pwapi
 
 import (
 	"context"
+	"encoding/json"
 
 	"pathwar.land/v2/go/pkg/errcode"
 	"pathwar.land/v2/go/pkg/pwdb"
+	"pathwar.land/v2/go/pkg/pwinit"
 )
 
 func (svc *service) ChallengeSubscriptionValidate(ctx context.Context, in *ChallengeSubscriptionValidate_Input) (*ChallengeSubscriptionValidate_Output, error) {
 	// validation
-	if in == nil || in.ChallengeSubscriptionID == 0 || in.Passphrase == "" {
+	if in == nil || in.ChallengeSubscriptionID == 0 || in.Passphrases == nil {
 		return nil, errcode.ErrMissingInput
 	}
 
@@ -32,14 +34,49 @@ func (svc *service) ChallengeSubscriptionValidate(ctx context.Context, in *Chall
 		return nil, errcode.ErrGetChallengeSubscription.Wrap(err)
 	}
 
-	// FIXME: check if passphrase is valid
-	// FIXME: check if passphrase_key wasn't already validated for this team ? or let it
-	// FIXME: check if challenge subscription is still open
+	// check if challenge subscription is still open
+	if challengeSubscription.Status != pwdb.ChallengeSubscription_Active {
+		return nil, errcode.ErrChallengeInactiveValidation.Wrap(err)
+	}
 
-	// create subscription
+	validPassphrases := map[int]bool{}
+	for _, instance := range challengeSubscription.SeasonChallenge.Instances {
+		var configData pwinit.InitConfig
+		err = json.Unmarshal(instance.GetInstanceConfig(), &configData)
+		if err != nil {
+			return nil, errcode.ErrParseInitConfig.Wrap(err)
+		}
+		tempPassphrases := map[int]bool{}
+		for index, challengePassphrase := range configData.Passphrases {
+			found := false
+			for _, userPassphrase := range in.Passphrases {
+				if userPassphrase == challengePassphrase {
+					found = true
+				}
+			}
+			tempPassphrases[index] = found
+		}
+		if len(tempPassphrases) > len(validPassphrases) {
+			validPassphrases = tempPassphrases
+		}
+	}
+	// FIXME: check if passphrase_key wasn't already validated for this team ? or let it
+
+	// if provided passphrase are not all valid
+	for _, valid := range validPassphrases {
+		if !valid {
+			return nil, errcode.ErrChallengeIncompleteValidation.Wrap(err)
+		}
+	}
+
+	// create validation
+	passphrases, err := json.Marshal(in.Passphrases)
+	if err != nil {
+		return nil, errcode.ErrChallengeJSONMarshalPassphrases.Wrap(err)
+	}
 	validation := pwdb.ChallengeValidation{
 		ChallengeSubscriptionID: in.ChallengeSubscriptionID,
-		Passphrase:              in.Passphrase,
+		Passphrases:             string(passphrases),
 		PassphraseKey:           "test",
 		AuthorID:                userID,
 		AuthorComment:           in.Comment,
