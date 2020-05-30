3d3de724af4bdcea12a785d99ce8b8d82c848c48
Lucas Germano <lucasgdev@gmail.com>
2020-05-30 20:33:28 -0300

    remove eslint ignore, pass to ignore patterns on single file

--- web/tests/jest.config.js
+++ web/tests/jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   transform: {
-    "^.+\\.jsx?$": `<rootDir>/config/jest/jest-preprocess.js`
+    "^.+\\.jsx?$": `<rootDir>/config/jest/jest-preprocess.js`,
   },
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/config/jest/__mocks__/file-mock.js`
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/config/jest/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
-    __PATH_PREFIX__: ``
+    __PATH_PREFIX__: ``,
   },
   testURL: `http://localhost`,
   setupFiles: [`<rootDir>/config/jest/loadershim.js`],
