3d81f8a90565965bec9c2ff8b95f199496bbea10
Lucas Santos <lucasgdev@gmail.com>
2020-06-03 09:58:03 -0300

    Checks for browser to prevent 404 blink on gatsby build

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from "gatsby";
 
+const browser = typeof window !== "undefined" && window;
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h4>
-        404 Page Not Found
-      </h4>
-      <Link to="/">Go back to homepage</Link>
-    </div>
+    browser && (
+      <div>
+        <h4>
+          404 Page Not Found
+        </h4>
+        <Link to="/">Go back to homepage</Link>
+      </div>
+    )
   );
 };
 
