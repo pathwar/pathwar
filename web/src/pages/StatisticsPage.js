c88659f66bbf5069d6ff4a4987d0243262d19f11
Lucas Santos <lucasgdev@gmail.com>
2020-07-03 18:13:10 -0300

    Move button Create Team to statistics page

--- web/src/pages/StatisticsPage.js
+++ web/src/pages/StatisticsPage.js
@@ -3,12 +3,20 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Grid, Page } from "tabler-react";
 import AllTeamsOnSeasonList from "../components/season/AllTeamsOnSeasonList";
-import { fetchAllSeasonTeams as fetchAllSeasonTeamsAction } from "../actions/seasons";
+import CreateTeamButton from "../components/team/CreateTeamButton";
+import {
+  fetchAllSeasonTeams as fetchAllSeasonTeamsAction,
+  createTeam as createTeamAction,
+} from "../actions/seasons";
 
 const StatisticsPage = () => {
   const dispatch = useDispatch();
   const activeSeason = useSelector(state => state.seasons.activeSeason);
+  const activeTeamInSeason = useSelector(
+    state => state.seasons.activeTeamInSeason
+  );
   const allTeamsOnSeason = useSelector(state => state.seasons.allTeamsOnSeason);
+  const dispatchCreateTeamAction = dispatch(createTeamAction);
 
   useEffect(
     state => {
@@ -23,6 +31,13 @@ const StatisticsPage = () => {
   return (
     <Page.Content title={`Statistics`}>
       <Grid.Row cards={true}>
+        <Grid.Col auto>
+          <CreateTeamButton
+            activeSeason={activeSeason}
+            createTeam={dispatchCreateTeamAction}
+            activeTeamInSeason={activeTeamInSeason}
+          />
+        </Grid.Col>
         <Grid.Col xs={12} sm={12} lg={6}>
           <AllTeamsOnSeasonList
             activeSeason={activeSeason}
