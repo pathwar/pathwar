c1d11c9d09d289db329f9b4b4326891dc55970d3
Lucas Santos <lucasgdev@gmail.com>
2020-07-03 18:15:04 -0300

    Remove teams statistics from challenges page

--- web/src/pages/ChallengesPage.js
+++ web/src/pages/ChallengesPage.js
@@ -4,16 +4,13 @@ import PropTypes from "prop-types";
 import { Page, Grid, Button } from "tabler-react";
 import { isNil } from "ramda";
 
-import AllTeamsOnSeasonList from "../components/season/AllTeamsOnSeasonList";
 import ChallengeList from "../components/challenges/ChallengeList";
 import ValidateCouponButton from "../components/coupon/ValidateCouponButton";
-import CreateTeamButton from "../components/team/CreateTeamButton";
 
 import {
   fetchChallenges as fetchChallengesAction,
   fetchAllSeasonTeams as fetchAllSeasonTeamsAction,
   buyChallenge as buyChallengeAction,
-  createTeam as createTeamAction,
 } from "../actions/seasons";
 
 class SeasonPage extends React.Component {
@@ -43,14 +40,7 @@ class SeasonPage extends React.Component {
   }
 
   render() {
-    const {
-      buyChallengeAction,
-      createTeamAction,
-      activeSeason,
-      activeChallenges,
-      allTeamsOnSeason,
-      activeTeamInSeason,
-    } = this.props;
+    const { buyChallengeAction, activeSeason, activeChallenges } = this.props;
     const name = activeSeason ? activeSeason.name : undefined;
 
     return (
@@ -64,19 +54,7 @@ class SeasonPage extends React.Component {
         </Grid.Row>
         <hr />
         <Grid.Row>
-          <Grid.Col xs={12} sm={3} lg={3}>
-            <h4>Teams</h4>
-            <CreateTeamButton
-              activeSeason={activeSeason}
-              createTeam={createTeamAction}
-              activeTeamInSeason={activeTeamInSeason}
-            />
-            <AllTeamsOnSeasonList
-              activeSeason={activeSeason}
-              allTeamsOnSeason={allTeamsOnSeason}
-            />
-          </Grid.Col>
-          <Grid.Col xs={12} sm={9} lg={9}>
+          <Grid.Col xs={12} sm={12} lg={12}>
             <h4>Challenges</h4>
             <ChallengeList
               challenges={activeChallenges}
@@ -97,8 +75,6 @@ SeasonPage.propTypes = {
 const mapStateToProps = state => ({
   activeSeason: state.seasons.activeSeason,
   activeChallenges: state.seasons.activeChallenges,
-  allTeamsOnSeason: state.seasons.allTeamsOnSeason,
-  activeTeamInSeason: state.seasons.activeTeamInSeason,
 });
 
 const mapDispatchToProps = {
@@ -106,7 +82,6 @@ const mapDispatchToProps = {
   fetchAllSeasonTeamsAction: seasonID => fetchAllSeasonTeamsAction(seasonID),
   buyChallengeAction: (seasonID, teamID) =>
     buyChallengeAction(seasonID, teamID),
-  createTeamAction: (seasonID, name) => createTeamAction(seasonID, name),
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SeasonPage);
