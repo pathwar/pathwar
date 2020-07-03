5bdaab3e78e7f7aa6a156cd1d59056e0b7faa169
Lucas Santos <lucasgdev@gmail.com>
2020-07-03 11:44:30 -0300

    Remove uneeded stamp card

--- web/src/pages/HomePage.js
+++ web/src/pages/HomePage.js
@@ -4,17 +4,11 @@ import { Page, Grid } from "tabler-react";
 
 import AllOrganizationsList from "../components/organizations/AllOrganizationsList";
 // import UserTeamsCard from "../components/teams/UserTeamsList";
-import TeamStatsStampCard from "../components/organizations/OrganizationStatsStampCard";
 
 const HomePage = () => {
   return (
     <Page.Content title="Dashboard">
       <Grid.Row cards={true}>
-        <Grid.Col xs={12} sm={12} lg={6}>
-          {/* <UserTeamsCard /> */}
-          <TeamStatsStampCard />
-        </Grid.Col>
-
         <Grid.Col xs={12} sm={12} lg={6}>
           <AllOrganizationsList />
         </Grid.Col>
