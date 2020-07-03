74d812694122adeb14e23cab382dbd5c6546f257
Lucas Santos <lucasgdev@gmail.com>
2020-07-03 17:26:46 -0300

    Inserting AllTeamsOnSeason component inside statistics page

--- web/src/pages/StatisticsPage.js
+++ web/src/pages/StatisticsPage.js
@@ -1,17 +1,25 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "gatsby";
-import { Button, Dimmer, Card, Grid } from "tabler-react";
-import styles from "../../styles/layout/loader.module.css";
+import { Grid, Page } from "tabler-react";
+import AllTeamsOnSeasonList from "../components/season/AllTeamsOnSeasonList";
 
 const StatisticsPage = props => {
-  const activeUserSession = useSelector(
-    state => state.userSession.activeUserSession
-  );
-  const activeTeam = useSelector(state => state.seasons.activeTeam);
+  const activeSeason = useSelector(state => state.seasons.activeSeason);
+  const allTeamsOnSeason = useSelector(state => state.seasons.allTeamsOnSeason);
 
-  return <h1>StatisticsPage</h1>;
+  return (
+    <Page.Content title={`Statistics`}>
+      <Grid.Row cards={true}>
+        <Grid.Col xs={12} sm={12} lg={6}>
+          <AllTeamsOnSeasonList
+            activeSeason={activeSeason}
+            allTeamsOnSeason={allTeamsOnSeason}
+          />
+        </Grid.Col>
+      </Grid.Row>
+    </Page.Content>
+  );
 };
 
 export default StatisticsPage;
