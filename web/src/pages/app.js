6000b2a794948cd7a4bd40c125526f4df6fe57fa
Lucas Santos <lucasgdev@gmail.com>
2020-07-04 14:21:52 -0300

    feat: insert theme on app, add plugin sass gatsby

--- web/src/pages/app.js
+++ web/src/pages/app.js
@@ -4,6 +4,10 @@ import { Helmet } from "react-helmet";
 import loadable from "@loadable/component";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Global } from "@emotion/core";
+import { ThemeProvider, useTheme } from "emotion-theming";
+import { lightTheme } from "../styles/themes";
+import { globalStyle } from "../styles/globalStyle";
 import HomePage from "./HomePage";
 import LogoutPage from "./LogoutPage";
 import ChallengesPage from "./ChallengesPage";
@@ -23,35 +27,53 @@ Sentry.init({
 });
 toast.configure();
 
-const App = () => (
-  <div>
-    <Helmet>
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-    </Helmet>
-    <SiteWrapper />
-    <Location>
-      {({ location }) => (
-        <Router location={location}>
-          <ProtectedRoute path="/app/home" component={HomePage} />
-          <ProtectedRoute path="/app/challenges" component={ChallengesPage} />
-          <ProtectedRoute path="/app/statistics" component={StatisticsPage} />
-          <ProtectedRoute
-            path="/app/team/:teamId"
-            component={TeamDetailsPage}
-          />
-          <ProtectedRoute
-            path="/app/challenge/:challengeId"
-            component={ChallengeDetailsPage}
-          />
-          <ProtectedRoute path="/app/settings" component={SettingsPage} />
-          <ProtectedRoute path="/app/logout" component={LogoutPage} />
-        </Router>
-      )}
-    </Location>
-  </div>
+const App = () => {
+  const currentTheme = useTheme();
+  return (
+    <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bungee&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Global styles={globalStyle(currentTheme)} />
+      <SiteWrapper />
+      <Location>
+        {({ location }) => (
+          <Router location={location}>
+            <ProtectedRoute path="/app/home" component={HomePage} />
+            <ProtectedRoute path="/app/challenges" component={ChallengesPage} />
+            <ProtectedRoute path="/app/statistics" component={StatisticsPage} />
+            <ProtectedRoute
+              path="/app/team/:teamId"
+              component={TeamDetailsPage}
+            />
+            <ProtectedRoute
+              path="/app/challenge/:challengeId"
+              component={ChallengeDetailsPage}
+            />
+            <ProtectedRoute path="/app/settings" component={SettingsPage} />
+            <ProtectedRoute path="/app/logout" component={LogoutPage} />
+          </Router>
+        )}
+      </Location>
+    </>
+  );
+};
+
+const ThemedApp = () => (
+  <ThemeProvider theme={lightTheme}>
+    <App />
+  </ThemeProvider>
 );
 
-export default App;
+export default ThemedApp;
