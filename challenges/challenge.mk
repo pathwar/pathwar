c5fce60b95b2be04d1ea35dae77ff8c95c7a7bbe
Manfred Touron <94029+moul@users.noreply.github.com>
2020-07-05 04:39:27 +0200

    chore: add new challenge helpers

--- challenges/challenge.mk
+++ challenges/challenge.mk
@@ -1,6 +1,19 @@
 PREFIX ?= pathwar/
 PATHWAR_OPTS ?=
 
+.PHONY: pathwar.run
+pathwar.run:
+	pathwar $(PATHWAR_OPTS) compose prepare --no-push ./ > pathwar-compose.yml
+	pathwar $(PATHWAR_OPTS) compose up --force-recreate pathwar-compose.yml
+
+.PHONY: pathwar.down
+pathwar.down:
+	pathwar --debug compose down $(notdir $(PWD))
+
+.PHONY: pathwar.ps
+pathwar.ps:
+	pathwar compose ps | grep $(notdir $(PWD))
+
 .PHONY: docker.build
 docker.build:
 	docker-compose build --pull
@@ -12,3 +25,7 @@ pathwar.prepare:
 .PHONY: pathwar-push
 pathwar.push:
 	pathwar $(PATHWAR_OPTS) compose prepare --prefix=$(PREFIX) .
+
+.PHONY: make.bump
+make.bump:
+	wget -O rules.mk https://raw.githubusercontent.com/tree/master/challenges/challenge.mk Makefile
