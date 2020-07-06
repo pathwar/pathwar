f2968c36afc8b0ecb4be5c7c64f8059d46662b37
Lucas Santos <lucasgdev@gmail.com>
2020-07-06 16:00:59 -0300

    Comment organizations list showing at homepage

--- web/src/pages/HomePage.js
+++ web/src/pages/HomePage.js
@@ -1,17 +1,17 @@
 import * as React from "react";
 
-import { Page, Grid } from "tabler-react";
+import { Page } from "tabler-react";
 
-import AllOrganizationsList from "../components/organizations/AllOrganizationsList";
+// import AllOrganizationsList from "../components/organizations/AllOrganizationsList";
 
 const HomePage = () => {
   return (
     <Page.Content title="Home">
-      <Grid.Row cards={true}>
+      {/* <Grid.Row cards={true}>
         <Grid.Col xs={12} sm={12} lg={6}>
           <AllOrganizationsList />
         </Grid.Col>
-      </Grid.Row>
+      </Grid.Row> */}
     </Page.Content>
   );
 };
