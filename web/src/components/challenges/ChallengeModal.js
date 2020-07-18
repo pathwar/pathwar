e720f53e3f30e02b69aef08a39f1913e459d653a
Lucas Santos <lucasgdev@gmail.com>
2020-07-18 18:18:59 -0300

    Create url pushState when opening and closing the modal

--- web/src/components/challenges/ChallengeModal.js
+++ web/src/components/challenges/ChallengeModal.js
@@ -3,7 +3,21 @@ import React from "react";
 import { Modal } from "react-responsive-modal";
 import ChallengeDetailsPage from "../../pages/ChallengeDetailsPage";
 
-const ChallengeModal = ({ open, onClose, challengeID }, ...rest) => {
+const browser = typeof window !== "undefined" && window;
+
+const ChallengeModal = (
+  { open, onClose: onCloseProps, challengeID },
+  ...rest
+) => {
+  if (open) {
+    browser && window.history.pushState(null, null, `?modal=${challengeID}`);
+  }
+
+  const onClose = () => {
+    browser && window.history.pushState(null, null, "/app/challenges");
+    onCloseProps && onCloseProps();
+  };
+
   return (
     <Modal
       open={open}
