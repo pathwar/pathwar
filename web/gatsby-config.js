73dc64743ea95c9f08bee48f03205484c2293218
Lucas Santos <lucasgdev@gmail.com>
2020-07-22 12:55:48 -0300

    Remove old unused sass lib, add query params lib

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
-    `gatsby-plugin-sass`,
+    `gatsby-plugin-use-query-params`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
