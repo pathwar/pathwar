fd5bae3982f6f3b3f4505f825bf90e74492e4ab2
Lucas Santos <lucasgdev@gmail.com>
2020-08-07 14:18:27 -0300

    Creating coupon api call

--- web/src/api/seasons.js
+++ web/src/api/seasons.js
@@ -56,3 +56,9 @@ export function postCloseChallenge(subscriptionID) {
     challenge_subscription_id: subscriptionID,
   });
 }
+
+//Coupon calls
+
+export function postCouponValidation(hash, teamID) {
+  return baseApi.post(`/coupon-validation`, { hash: hash, team_id: teamID });
+}
