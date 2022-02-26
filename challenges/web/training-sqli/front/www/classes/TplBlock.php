cd01c00749b0be5c6a108123d9088ba0533274dd
Gnieark <gnieark@free.fr>
2022-02-26 23:21:56 +0100

    TplBlock code Factor improve

--- challenges/web/training-sqli/front/www/classes/TplBlock.php
+++ challenges/web/training-sqli/front/www/classes/TplBlock.php
@@ -311,7 +311,8 @@ class TplBlock
      */
     public function applyTplFile($file)
     {
-        if (! $tplStr = file_get_contents($file)) {
+        $tplStr = file_get_contents($file));
+        if ( $tplStr = == false ) {
             throw new \UnexpectedValueException("Cannot read given file $file");
         }
 
