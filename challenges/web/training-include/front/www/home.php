72d31fdf591920b889f9f8723836dfdb1fb8e8b9
Gnieark <gineark@free.fr>
2022-02-27 23:37:11 +0100

    Training-include translation code

--- challenges/web/training-include/front/www/home.php
+++ challenges/web/training-include/front/www/home.php
@@ -1,7 +1,5 @@
-<p>Welcome to the Pathwar <b>Include</b> Training</p>
-
-<p>You'll want to play with this stuff to build a handy skillset for the challenges ahead...</p>
-
-<p>Enjoy!</p>
-
-<p><a href="?page=include1">Click here to continue</a></p>
+<?php
+echo ("<p>" . _('Welcome to the Pathwar <b>Include</b> Training') . "</p>"
+    ."<p>" . _('You\'ll want to play with this stuff to build a handy skillset for the challenges ahead...') . "</p>"
+    ."<p>" .  _('Enjoy!') . "</p>"
+    ."<p><a href=\"?page=include1\">" . _('Click here to continue') . "</a></p>");
--- challenges/web/training-include/front/www/include1.php
+++ challenges/web/training-include/front/www/include1.php
@@ -1,7 +1,5 @@
-
-<p>So, we're seeing a lot of websites using an <a href="http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller">MVC</a> architecture.</p>
-<p>A lot of them are homemade, with serious but subtle security flaws.</p>
-
-<p>Here you'll learn how to exploit them.</p>
-
-<p><a href="/index.php?page=include2">Continue...</a></p>
+<?php
+echo ("<p>" . _('So, we\'re seeing a lot of websites using an <a href="http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller">MVC</a> architecture. ') . "</p>"
+     . "<p>" . _('A lot of them are homemade, with serious but subtle security flaws.') ."</p>"
+     . "<p>" . _('Here you\'ll learn how to exploit them.') . "</p>"
+     . "<p><a href=\"/index.php?page=include2\">" . _('Continue...') ."</a></p>");
