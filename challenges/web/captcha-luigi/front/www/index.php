7eddd2375dec8ceda6c3ac468016b69e279abc88
Gnieark <gnieark@free.fr>
2022-03-20 23:27:07 +0100

    feat: Captcha Luigi multilanguage (#728)

--- challenges/web/captcha-luigi/front/www/index.php
+++ challenges/web/captcha-luigi/front/www/index.php
@@ -1,5 +1,26 @@
 <?php
   session_start();
+  require_once ("LocaleManager.php");
+  $currentLocale = new LocaleManager();
+  ?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+      <meta charset="UTF-8">
+      <title><?php echo _('Captcha Luigi'); ?></title>
+      <link rel="stylesheet" href="/style.css">
+  </head>
+  <body>
+      <header>
+          <h1><?php echo _('Captcha Luigi'); ?></h1>
+      </header>
+      <nav class="topright">
+          <?php echo $currentLocale->get_locale_form(); ?>
+      </nav>
+      <section class="content">
+      <article>
+
+<?php
   if (!isset($_SESSION['score'])) {
     $_SESSION['score'] = 0;
   }
@@ -8,24 +29,26 @@
     if ($_POST['password'] == $_SESSION['password']) {
       $_SESSION['score']++;
       unset($_SESSION['password']);
-      echo "<p>You win !</p>";
+      echo "<p>" . _('You win !') . "</p>";
     } else {
-      echo "<p>You lose !</p>";
+      echo "<p>" . _('You lose !') ."</p>";
     }
   }
 
-  echo '<p>Score: ', $_SESSION['score'], '/100';
+  echo '<p>' . _('Score:') . ' ', $_SESSION['score'], '/100';
 
   if ($_SESSION['score'] >= 100) {
-    echo "<p>The passphrase is: {passphrase}</p>";
+    echo "<p>" . _('The passphrase is:') . " {passphrase}</p>";
   }
 ?>
-<div>
-  <img src="./captcha.php" />
-</div>
-<div>
+  <p><img src="./captcha.php" /></p>
+</article>
+<article>
   <form method="POST">
     <input name="password" type="text" />
     <input type="submit" />
   </form>
-</div>
+</article>
+</section>
+</body>
+</html>
