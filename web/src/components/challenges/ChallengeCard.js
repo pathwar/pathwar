e553090051c7321558d5d70d111e33fa810b54be
Manfred Touron <94029+moul@users.noreply.github.com>
2022-03-21 22:41:50 +0100

    chore: increase challenges density

--- web/src/components/challenges/ChallengeCard.js
+++ web/src/components/challenges/ChallengeCard.js
@@ -23,7 +23,7 @@ const cardWrapper = isClosed => css`
   align-items: center;
   box-shadow: 0px 5px 20px 0px rgba(7, 42, 68, 0.1);
   margin-bottom: 0.5rem;
-  padding: 3rem 1rem;
+  padding: 1rem 1rem;
   border-radius: 8px;
   min-height: 374px;
   width: 100%;
