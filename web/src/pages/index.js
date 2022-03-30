80bc7009c13707b6639723d36f4927614e7cf1fb
Lucas Germano <lucasgdev@gmail.com>
2022-03-30 17:12:47 -0300

    fix: add analytics script

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,16 +1,32 @@
-/* eslint-disable react/display-name */
 import React from "react";
 import { ThemeProvider } from "emotion-theming";
+import { Helmet } from "react-helmet";
 import { lightTheme } from "../styles/themes";
 
 import { App } from "./app";
 
-export default () => {
+const Index = () => {
   const themeToUse = lightTheme;
 
   return (
-    <ThemeProvider theme={themeToUse}>
-      <App />
-    </ThemeProvider>
+    <>
+      <Helmet>
+        <script async defer src="https://sa.moul.io/latest.js"></script>
+        <noscript>
+          {`
+          <img
+            src="https://queue.simpleanalyticscdn.com/noscript.gif"
+            alt=""
+            referrerpolicy="no-referrer-when-downgrade"
+          />
+          `}
+        </noscript>
+      </Helmet>
+      <ThemeProvider theme={themeToUse}>
+        <App />
+      </ThemeProvider>
+    </>
   );
 };
+
+export default Index;
