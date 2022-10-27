fef85bd16c81bcfb72be2446040abe29a32a4a8c
Mikatech <mikael.vallenet@epitech.eu>
2022-10-27 14:50:51 +0200

    feat: switch season with input instead of dropdown

---
+++ web/src/components/season/SwitchSeasonInput.js
@@ -0,0 +1,43 @@
+import { FormattedMessage } from "react-intl";
+import * as React from "react";
+import { Card } from "tabler-react";;
+import { useDispatch } from "react-redux";
+import { Button } from "tabler-react";
+import {setSwitchSeason} from "../../actions/seasons";
+
+const SwitchSeasonInput = () => {
+
+  const [season, setSeason] = React.useState('');
+
+  const dispatch = useDispatch();
+  const setActiveSeasonDispatch = seasonID => dispatch(setSwitchSeason(seasonID));
+
+  const SwitchSeason = async seasonID => {
+    setActiveSeasonDispatch(seasonID).then(response => {
+      return response;
+    });
+  };
+
+  return (
+    <Card>
+      <Card.Header>
+        <Card.Title>
+          Season switch
+        </Card.Title>
+      </Card.Header>
+        <input
+          value={season}
+          onChange={e => setSeason(e.target.value)}
+        />
+        <Button.List>
+          <Button
+            onClick={() => SwitchSeason(season)}
+            color="primary"
+          >Switch
+          </Button>
+        </Button.List>
+    </Card>
+  );
+};
+
+export default SwitchSeasonInput;
