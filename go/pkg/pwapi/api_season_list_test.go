0ab0aecb4acc0c89c3e95b6d6006de016a989fac
Mikatech <mikael.vallenet@epitech.eu>
2022-11-02 11:32:29 +0100

    tests(api): add to season list

--- go/pkg/pwapi/api_season_list_test.go
+++ go/pkg/pwapi/api_season_list_test.go
@@ -2,9 +2,12 @@ package pwapi
 
 import (
 	"context"
+	"pathwar.land/pathwar/v2/go/pkg/pwdb"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"pathwar.land/pathwar/v2/go/internal/testutil"
-	"testing"
 )
 
 func TestService_SeasonList(t *testing.T) {
@@ -15,4 +18,31 @@ func TestService_SeasonList(t *testing.T) {
 	// fetch user session to ensure account is created
 	_, err := svc.UserGetSession(ctx, nil)
 	require.NoError(t, err)
+
+	tests := []struct {
+		name              string
+		input             *SeasonList_Input
+		expectedErr       error
+		ExpectedStatus    pwdb.Season_Status
+		ExpectedGlobal    bool
+		ExpectedIsTesting bool
+	}{
+		{"all-seasons", &SeasonList_Input{}, nil, pwdb.Season_Started, true, false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ret, err := svc.SeasonList(ctx, test.input)
+			testSameErrcodes(t, "", test.expectedErr, err)
+			if err != nil {
+				return
+			}
+
+			for _, season := range ret.Items {
+				assert.Equal(t, test.ExpectedStatus, season.Status)
+				assert.Equal(t, test.ExpectedGlobal, season.IsGlobal)
+				assert.Equal(t, test.ExpectedIsTesting, season.IsTesting)
+			}
+		})
+	}
 }
