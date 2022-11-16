1925366983adffcd4efe227b6dae56e94e716921
Mikatech <mikael.vallenet@epitech.eu>
2022-11-16 16:11:24 +0100

    feat(compute-score): split all validations per challenges

--- go/pkg/pwes/compute.go
+++ go/pkg/pwes/compute.go
@@ -4,13 +4,30 @@ import (
 	"context"
 	"fmt"
 	"pathwar.land/pathwar/v2/go/pkg/pwapi"
+	"pathwar.land/pathwar/v2/go/pkg/pwdb"
 )
 
+type challengeValidation struct {
+	validations []*pwdb.Activity
+}
+
 func Compute(ctx context.Context, apiClient *pwapi.HTTPClient) error {
-	activities, err := apiClient.AdminListActivities(ctx, &pwapi.AdminListActivities_Input{FilteringPreset: "validations"})
+	res, err := apiClient.AdminListActivities(ctx, &pwapi.AdminListActivities_Input{FilteringPreset: "validations"})
 	if err != nil {
 		return err
 	}
-	fmt.Println(activities)
+	activities := res.GetActivities()
+
+	//TODO: Perhaps a better way to split validations per challenges
+	challenges := make(map[int64]challengeValidation)
+	for _, activity := range activities {
+		if _, ok := challenges[activity.ChallengeID]; ok {
+			challenges[activity.ChallengeID] = challengeValidation{append(challenges[activity.ChallengeID].validations, activity)}
+		} else {
+			challenges[activity.ChallengeID] = challengeValidation{[]*pwdb.Activity{activity}}
+		}
+	}
+
+	fmt.Println(challenges)
 	return nil
 }
