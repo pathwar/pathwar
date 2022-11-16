de5c86fb0e5fceb3c4ebb34355a30edcb12e3eef
Mikatech <mikael.vallenet@epitech.eu>
2022-11-16 16:24:15 +0100

    feat(compute-score): compute score of each challenge

--- go/pkg/pwes/compute.go
+++ go/pkg/pwes/compute.go
@@ -9,8 +9,10 @@ import (
 
 type challengeValidation struct {
 	validations []*pwdb.Activity
-	score       uint64
+	score       int
 }
+
+//TODO: Don't forget to test the function
 
 func Compute(ctx context.Context, apiClient *pwapi.HTTPClient) error {
 	res, err := apiClient.AdminListActivities(ctx, &pwapi.AdminListActivities_Input{FilteringPreset: "validations"})
@@ -23,6 +25,12 @@ func Compute(ctx context.Context, apiClient *pwapi.HTTPClient) error {
 	challenges := make(map[int64]challengeValidation)
 	for _, activity := range activities {
 		challenges[activity.ChallengeID] = challengeValidation{append(challenges[activity.ChallengeID].validations, activity), 0}
+	}
+
+	// TODO: Apply a better function: compute score : 1 / (x/10 + 1) * 95 + 5
+	for _, challenge := range challenges {
+		nbValidations := len(challenge.validations)
+		challenge.score = 1/(nbValidations/10+1)*95 + 5
 	}
 
 	fmt.Println(challenges)
