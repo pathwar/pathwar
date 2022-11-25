2fff7e2a2e13e7cabb83585307387ec79189a9ca
Mikatech <mikael.vallenet@epitech.eu>
2022-11-25 17:32:16 +0100

    feat(api): add adminListSeasonChallenges func

---
+++ go/pkg/pwapi/api_admin-list-season-challenges.go
@@ -0,0 +1,14 @@
+package pwapi
+
+import (
+	"context"
+	"pathwar.land/pathwar/v2/go/pkg/errcode"
+)
+
+func (svc *service) AdminListSeasonChallenges(ctx context.Context, in *AdminListSeasonChallenges_Input) (*AdminListSeasonChallenges_Output, error) {
+	if !isAdminContext(ctx) {
+		return nil, errcode.ErrRestrictedArea
+	}
+
+	return &AdminListSeasonChallenges_Output{}, nil
+}
