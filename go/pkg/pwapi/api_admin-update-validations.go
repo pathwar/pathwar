2e870e7fc23a86b82849905788d99fa8dad83dd6
Mikatech <mikael.vallenet@epitech.eu>
2022-11-25 15:49:20 +0100

    feat: add api route to update nb_validations of season challenge

--- go/pkg/pwapi/api_admin-update-validations.go
+++ go/pkg/pwapi/api_admin-update-validations.go
@@ -3,12 +3,25 @@ package pwapi
 import (
 	"context"
 
+	"pathwar.land/pathwar/v2/go/pkg/pwdb"
+
 	"pathwar.land/pathwar/v2/go/pkg/errcode"
 )
 
 func (svc *service) AdminUpdateValidations(ctx context.Context, in *AdminUpdateValidations_Input) (*AdminUpdateValidations_Output, error) {
 	if !isAdminContext(ctx) {
 		return nil, errcode.ErrRestrictedArea
+	}
+
+	if in == nil || in.SeasonChallenge == nil {
+		return nil, errcode.ErrMissingInput
+	}
+
+	for _, challenge := range in.SeasonChallenge {
+		err := svc.db.Model(&pwdb.SeasonChallenge{}).Update("nb_validations", challenge.NbValidations).Error
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return nil, nil
