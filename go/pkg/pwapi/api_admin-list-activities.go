50151665c1e8076a5f6eb7b31fd5357f62a508bc
Mikatech <mikael.vallenet@epitech.eu>
2022-12-21 19:39:15 +0100

    fix(api): delete useless field season_challenge_id in listActivities route

--- go/pkg/pwapi/api_admin-list-activities.go
+++ go/pkg/pwapi/api_admin-list-activities.go
@@ -37,14 +37,11 @@ func (svc *service) AdminListActivities(ctx context.Context, in *AdminListActivi
 		fmt.Println("TEST, in.Limit")
 		req = req.Limit(in.Limit)
 	}
-	if in.Since != nil && !in.Since.IsZero() {
+	if in.Since != nil {
 		req = req.Where("created_at >= ?", *in.Since)
 	}
 	if in.To != nil && !in.To.IsZero() {
 		req = req.Where("created_at <= ?", *in.To)
-	}
-	if in.SeasonChallengeID != "" {
-		req = req.Where("season_challenge_id = ?", in.SeasonChallengeID)
 	}
 	switch in.FilteringPreset {
 	case "default", "":
