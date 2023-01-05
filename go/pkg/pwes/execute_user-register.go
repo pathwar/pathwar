24e53fe3c38be75e5da0f557552e3b9a29343559
Mikatech <mikael.vallenet@epitech.eu>
2023-01-05 14:14:30 +0100

    feat(es): add all execute() functions for each type of events

--- go/pkg/pwes/execute_user-register.go
+++ go/pkg/pwes/execute_user-register.go
@@ -1,1 +1,14 @@
 package pwes
+
+import (
+	"context"
+	"pathwar.land/pathwar/v2/go/pkg/errcode"
+	"pathwar.land/pathwar/v2/go/pkg/pwapi"
+)
+
+func (e EventUserRegister) execute(ctx context.Context, apiClient *pwapi.HTTPClient) error {
+	if apiClient == nil {
+		return errcode.ErrMissingInput
+	}
+	return nil
+}
