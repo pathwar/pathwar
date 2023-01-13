dce9708f76c5105aceb7ac683e0c5685c18f80d1
Mikatech <mikael.vallenet@epitech.eu>
2023-01-13 17:38:48 +0100

    feat(api): allo to create organization without create a team

--- go/pkg/pwapi/api_organization_create.go
+++ go/pkg/pwapi/api_organization_create.go
@@ -11,7 +11,42 @@ func (svc *service) OrganizationCreate(ctx context.Context, in *OrganizationCrea
 	if in == nil || in.Name == "" {
 		return nil, errcode.ErrMissingInput
 	}
-	return &OrganizationCreate_Output{
-		Organization: &pwdb.Organization{},
-	}, nil
+
+	userID, err := userIDFromContext(ctx, svc.db)
+	if err != nil {
+		return nil, errcode.ErrUnauthenticated.Wrap(err)
+	}
+
+	in.Name = normalizeName(in.Name)
+	if isReservedName(in.Name) {
+		return nil, errcode.ErrReservedName
+	}
+
+	// check for existing organization with that name
+	var count int
+	err = svc.db.Model(pwdb.Organization{}).Where(pwdb.Organization{Name: in.Name}).Count(&count).Error
+	if err != nil || count != 0 {
+		return nil, errcode.ErrCheckOrganizationUniqueName.Wrap(err)
+	}
+
+	// create new organization
+	organization := pwdb.Organization{
+		Name: in.Name,
+		Members: []*pwdb.OrganizationMember{
+			{
+				UserID: userID,
+				Role:   pwdb.OrganizationMember_Owner,
+			},
+		},
+		DeletionStatus: pwdb.DeletionStatus_Active,
+		// GravatarURL
+		// Locale
+	}
+	err = svc.db.Create(&organization).Error
+	if err != nil {
+		return nil, errcode.ErrCreateOrganization.Wrap(err)
+	}
+
+	ret := &OrganizationCreate_Output{Organization: &organization}
+	return ret, nil
 }
