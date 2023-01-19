df2096728de7476301831bd00cafc5ab6c338c5a
Mikatech <mikael.vallenet@epitech.eu>
2023-01-19 15:59:29 +0100

    feat(api): add API route to get an organization

--- go/pkg/pwapi/api_organization-get.go
+++ go/pkg/pwapi/api_organization-get.go
@@ -1,1 +1,30 @@
 package pwapi
+
+import (
+	"context"
+	"pathwar.land/pathwar/v2/go/pkg/errcode"
+	"pathwar.land/pathwar/v2/go/pkg/pwdb"
+)
+
+func (svc *service) OrganizationGet(ctx context.Context, in *OrganizationGet_Input) (*OrganizationGet_Output, error) {
+	if in == nil || in.OrganizationID == 0 {
+		return nil, errcode.ErrMissingInput
+	}
+
+	var item pwdb.Organization
+	err := svc.db.
+		Preload("Members").
+		Where(pwdb.Organization{
+			ID:             in.OrganizationID,
+			DeletionStatus: pwdb.DeletionStatus_Active,
+		}).
+		First(&item).
+		Error
+	if err != nil {
+		return nil, errcode.ErrGetOrganization.Wrap(err)
+	}
+
+	ret := OrganizationGet_Output{}
+
+	return &ret, nil
+}
