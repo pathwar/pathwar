f0701c633df1f894c77701d05b5e155b662c7414
Mikatech <mikael.vallenet@epitech.eu>
2023-01-25 11:51:03 +0100

    feat(api): add unit test to organization create

---
+++ go/pkg/pwapi/api_organization-create_test.go
@@ -0,0 +1,37 @@
+package pwapi
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"pathwar.land/pathwar/v2/go/internal/testutil"
+	"pathwar.land/pathwar/v2/go/pkg/errcode"
+	"testing"
+)
+
+func TestService_OrganizationCreate(t *testing.T) {
+	svc, cleanup := TestingService(t, ServiceOpts{Logger: testutil.Logger(t)})
+	defer cleanup()
+	ctx := testingSetContextToken(context.Background(), t)
+
+	_, err := svc.UserGetSession(ctx, nil)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		input       *OrganizationCreate_Input
+		expectedErr error
+	}{
+		{"empty", &OrganizationCreate_Input{}, errcode.ErrMissingInput},
+		{"nil", nil, errcode.ErrMissingInput},
+		{"reserved name", &OrganizationCreate_Input{Name: "pathwar"}, errcode.ErrReservedName},
+		{"my_organization", &OrganizationCreate_Input{Name: "my_organization"}, nil},
+		{"my_organization", &OrganizationCreate_Input{Name: "my_organization"}, errcode.ErrCheckOrganizationUniqueName},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := svc.OrganizationCreate(ctx, test.input)
+			testSameErrcodes(t, "", test.expectedErr, err)
+		})
+	}
+}
