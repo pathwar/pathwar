db1ebf3c0cf79e7367bce97b0ed7d1290fc84da3
Mikatech <mikael.vallenet@epitech.eu>
2023-01-25 14:14:35 +0100

    feat(api): define prototype of route invite organizations

--- go/pkg/pwapi/api_organization-accept-invite.go
+++ go/pkg/pwapi/api_organization-accept-invite.go
@@ -1,1 +1,7 @@
 package pwapi
+
+import "context"
+
+func (svc *service) OrganizationAcceptInvite(ctx context.Context, in *OrganizationAcceptInvite_Input) (*OrganizationAcceptInvite_Output, error) {
+	panic("not implemented")
+}
--- go/pkg/pwapi/api_organization-send-invite.go
+++ go/pkg/pwapi/api_organization-send-invite.go
@@ -1,1 +1,7 @@
 package pwapi
+
+import "context"
+
+func (svc *service) OrganizationSendInvite(ctx context.Context, in *OrganizationSendInvite_Input) (*OrganizationSendInvite_Output, error) {
+	panic("not implemented")
+}
