2f68a275364265a071df0ee81d88c5125e8ddf3b
Mikatech <mikael.vallenet@epitech.eu>
2023-02-22 15:25:00 +0100

    feat(web): add format msg translation into table of orga invitations

--- web/src/components/organization/UserOrganizationsInvitationsList.js
+++ web/src/components/organization/UserOrganizationsInvitationsList.js
@@ -4,11 +4,31 @@ import PropTypes from "prop-types";
 
 // import styles from "./style.module.css";
 import { FormattedMessage } from "react-intl";
+import {Link} from "gatsby";
 
 const OrganizationsInvitationsRows = ({ organizationsInvitations }) => {
   return organizationsInvitations.map((item, idx) => {
     return (
       <Table.Row key={item.id}>
+        <Table.Col alignContent="center">{idx + 1}</Table.Col>
+
+        <Table.Col alignContent="center"
+        >
+          <Avatar
+            className="mr-2"
+            imageURL={`${item.gravatar_url}?d=identicon`}
+          />
+          <Link
+            className="link"
+            to={"/organization/" + item.id}
+            activeStyle={{
+              fontWeight: "bold",
+              color: currentTheme.colors.primary,
+            }}
+          >
+            {item.name}
+          </Link>
+        </Table.Col>
         <Table.Col alignContent="center">{idx + 1}</Table.Col>
       </Table.Row>
     );
@@ -30,6 +50,9 @@ const UserOrganizationsInvitationsList = ({ userOrganizationsInvitationsList }) 
           <Table.Row>
             <Table.ColHeader alignContent="center">
               <FormattedMessage id="UserOrganizationsList.rank" />
+            </Table.ColHeader>
+            <Table.ColHeader alignContent="center">
+              <FormattedMessage id="UserOrganizationsList.organization" />
             </Table.ColHeader>
           </Table.Row>
         </Table.Header>
