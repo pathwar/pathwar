861b1c42440290f95ca225641e5caa48fc2514e9
Mikatech <mikael.vallenet@epitech.eu>
2023-03-10 17:46:51 +0100

    feat(go): add UserID input for admin in SeasonChallengeGet api route

--- go/pkg/pwapi/api_season-challenge-get.go
+++ go/pkg/pwapi/api_season-challenge-get.go
@@ -14,6 +14,14 @@ func (svc *service) SeasonChallengeGet(ctx context.Context, in *SeasonChallengeG
 	}
 
 	userID, err := userIDFromContext(ctx, svc.db)
+	if in.UserID != 0 {
+		if isAdminContext(ctx) {
+			return nil, errcode.ErrRestrictedArea
+		} else {
+			userID = in.UserID
+		}
+	}
+
 	if err != nil {
 		return nil, errcode.ErrUnauthenticated.Wrap(err)
 	}
@@ -22,7 +30,6 @@ func (svc *service) SeasonChallengeGet(ctx context.Context, in *SeasonChallengeG
 	if err != nil {
 		return nil, errcode.ErrGetSeasonFromSeasonChallenge.Wrap(err)
 	}
-
 	team, err := userTeamForSeason(svc.db, userID, season.ID)
 	if err != nil {
 		return nil, errcode.ErrGetUserTeamFromSeason.Wrap(err)
