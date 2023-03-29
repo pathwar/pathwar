464f710f76bf8f6b7fa2fc8fd406d972e7d5cd58
Mikatech <mikael.vallenet@epitech.eu>
2023-03-29 14:53:25 +0200

    feat: add function ton apply default values on seasons rules struct

--- go/pkg/pwapi/seasons_rules.go
+++ go/pkg/pwapi/seasons_rules.go
@@ -1,9 +1,21 @@
 package pwapi
 
+import "time"
+
 type SeasonsRules struct {
 	StartDatetime       int64  `yaml:"start_datetime"`
 	EndDatetime         int64  `yaml:"end_datetime"`
 	LimitPlayersPerTeam int32  `yaml:"limit_players_per_team"`
 	LimitTotalTeams     int32  `yaml:"limit_total_teams"`
 	EmailDomain         string `yaml:"email_domain"`
+}
+
+func NewSeasonsRules() SeasonsRules {
+	return SeasonsRules{
+		StartDatetime:       time.Now().Unix(),
+		EndDatetime:         0,
+		LimitPlayersPerTeam: 0,
+		LimitTotalTeams:     0,
+		EmailDomain:         "",
+	}
 }
