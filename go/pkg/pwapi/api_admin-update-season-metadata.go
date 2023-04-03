be2bfc5c2f658857c0833dcea02e73bf6fff324c
Mikatech <mikael.vallenet@epitech.eu>
2023-04-03 17:42:01 +0200

    feat(api): add save in db in admin update season metadata api route

--- go/pkg/pwapi/api_admin-update-season-metadata.go
+++ go/pkg/pwapi/api_admin-update-season-metadata.go
@@ -2,10 +2,10 @@ package pwapi
 
 import (
 	"context"
-
 	"pathwar.land/pathwar/v2/go/pkg/errcode"
 )
 
+// AdminUpdateSeasonMetadata Actually, this update only the subscription field of the season object but it could be extended to update other fields in the future
 func (svc *service) AdminUpdateSeasonMetadata(ctx context.Context, in *AdminUpdateSeasonMetadata_Input) (*AdminUpdateSeasonMetadata_Output, error) {
 	if !isAdminContext(ctx) {
 		return nil, errcode.ErrRestrictedArea
@@ -15,13 +15,10 @@ func (svc *service) AdminUpdateSeasonMetadata(ctx context.Context, in *AdminUpda
 		return nil, errcode.ErrMissingInput
 	}
 
-	/*	updates := pwdb.Season{
-			IsPublic: in.Season.IsPublic,
-		}
-		err := svc.db.Model(in.Season).Update(&updates).Error
-		if err != nil {
-			return nil, err
-		}*/
+	err := svc.db.Model(in.Season).Update("subscription", in.Season.Subscription).Error
+	if err != nil {
+		return nil, err
+	}
 
-	return &AdminUpdateSeasonMetadata_Output{}, nil
+	return &AdminUpdateSeasonMetadata_Output{Season: in.Season}, nil
 }
