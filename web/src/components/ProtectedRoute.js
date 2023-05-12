23b9faa8e2defefc978545ebf3abf2708d22b7f5
Mikatech <mikael.vallenet@epitech.eu>
2023-05-12 11:46:35 +0200

    feat: add auth0

--- web/src/components/ProtectedRoute.js
+++ web/src/components/ProtectedRoute.js
@@ -1,23 +1,33 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Dimmer } from "tabler-react";
-import {setAuthSession} from "../actions/userSession";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const dispatch = useDispatch();
   const userSession = useSelector(state => state.userSession);
+  const { isLoading, isAuthenticated, loginWithRedirect, getIdTokenClaims } = useAuth0()
 
-  useEffect(() => {
-    const { access_token } = userSession;
-    if (!access_token) {
-      dispatch(setAuthSession());
-    }
-  }, []);
+  if (!isLoading && !isAuthenticated) {
+    loginWithRedirect();
+  } else if (!isLoading && isAuthenticated) {
+    getIdTokenClaims().then((claims) => {
+      console.log(claims.__raw);
+    });
+  }
+  // useEffect(() => {
+  //   const { access_token } = userSession;
+  //   if (!access_token) {
+  //     dispatch(setAuthSession());
+  //   }
+  // }, []);
 
-    if (userSession.isAuthenticated) {
-      return <Component {...rest} />;
-    }
-
+    // if (userSession.isAuthenticated) {
+    //   return (
+    //   <Component {...rest} />
+    //   );
+    // }
+  console.log(isLoading, isAuthenticated)
   return <Dimmer active loader />;
 };
 
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "emotion-theming";
 import { lightTheme } from "../styles/themes";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import { App } from "./app";
 
@@ -8,9 +9,17 @@ const Index = () => {
   const themeToUse = lightTheme;
 
   return (
-    <ThemeProvider theme={themeToUse}>
-      <App />
-    </ThemeProvider>
+    <Auth0Provider
+    domain="dev-5ccwzy8qtcsjsnpf.us.auth0.com"
+    clientId="bJpLWOLTRseEVfM9kvFhKfi9wUBmm8Gh"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+      <ThemeProvider theme={themeToUse}>
+        <App />
+      </ThemeProvider>
+    </Auth0Provider>
   );
 };
 
