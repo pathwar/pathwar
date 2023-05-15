111aa833099deb876416984a6a64eb1aedc204e9
Mikatech <mikael.vallenet@epitech.eu>
2023-05-15 15:27:43 +0200

    feat: auth0 get token

--- web/src/components/ProtectedRoute.js
+++ web/src/components/ProtectedRoute.js
@@ -3,16 +3,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { Dimmer } from "tabler-react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+
+//TODO: update redux state with isAuthenticated value
+//TODO: dispatch action to update redux state with userSession
+//TODO: Then if userSession.isAuthenticated is true & userSession.accesToken is set, render the component
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const dispatch = useDispatch();
   const userSession = useSelector(state => state.userSession);
-  const { isLoading, isAuthenticated, loginWithRedirect, getIdTokenClaims } = useAuth0()
+  const { isLoading, isAuthenticated, loginWithRedirect, getAccessTokenSilently } = useAuth0()
 
   if (!isLoading && !isAuthenticated) {
     loginWithRedirect();
   } else if (!isLoading && isAuthenticated) {
-    getIdTokenClaims().then((claims) => {
-      console.log(claims.__raw);
+    getAccessTokenSilently().then((token) => {
+      console.log(token.__raw);
     });
   }
   // useEffect(() => {
