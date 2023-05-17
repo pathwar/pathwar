797957b32892db1858975a65ebfcc9491737da0a
Mikatech <mikael.vallenet@epitech.eu>
2023-05-17 02:02:54 +0200

    feat(web): use id token of auth0 instead of access token in a way to have claims

--- web/src/components/ProtectedRoute.js
+++ web/src/components/ProtectedRoute.js
@@ -4,10 +4,6 @@ import { Dimmer } from "tabler-react";
 import { useAuth0 } from "@auth0/auth0-react";
 import {setAuthSession} from "../actions/userSession";
 
-
-//TODO: update redux state with isAuthenticated value
-//TODO: dispatch action to update redux state with userSession
-//TODO: Then if userSession.isAuthenticated is true & userSession.accesToken is set, render the component
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   const dispatch = useDispatch();
   const userSession = useSelector(state => state.userSession);
@@ -15,21 +11,21 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
     isLoading,
     isAuthenticated,
     loginWithRedirect,
-    getAccessTokenSilently,
+    getIdTokenClaims,
   } = useAuth0()
 
   if (!isLoading && !isAuthenticated) {
     loginWithRedirect()
-      .then(() => {getAccessTokenSilently()
+      .then(() => {getIdTokenClaims()
         .then((token) => {
-          console.log(token);
-          dispatch(setAuthSession(token))
+          console.log(token.__raw);
+          dispatch(setAuthSession(token.__raw))
         })
       })
   } else if (!isLoading && isAuthenticated && !userSession.accessToken) {
-     getAccessTokenSilently().then((token) => {
-        console.log(token);
-        dispatch(setAuthSession(token))
+    getIdTokenClaims().then((token) => {
+        console.log(token.__raw);
+        dispatch(setAuthSession(token.__raw))
      })
   }
 
