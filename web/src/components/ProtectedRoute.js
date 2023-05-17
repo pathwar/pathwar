5f353631ee3c098530de52c08cce1f4f7b06e99d
Mikatech <mikael.vallenet@epitech.eu>
2023-05-17 02:03:55 +0200

    feat(web): delete unused import

--- web/src/components/ProtectedRoute.js
+++ web/src/components/ProtectedRoute.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Dimmer } from "tabler-react";
 import { useAuth0 } from "@auth0/auth0-react";
